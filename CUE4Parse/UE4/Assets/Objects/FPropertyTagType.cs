d8b3bdedc51ef84de66e4b96e4544c2461e9bd9b
Fabian <mcdtloeser@gmail.com>
2020-09-26 21:43:10 +0200

    beginnings for handling FPropertyTagType's + StringUtils

--- CUE4Parse/UE4/Assets/Objects/FPropertyTagType.cs
+++ CUE4Parse/UE4/Assets/Objects/FPropertyTagType.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using CUE4Parse.UE4.Assets.Exports;
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Core.i18N;
 using CUE4Parse.UE4.Objects.UObject;
+using CUE4Parse.Utils;
 
 namespace CUE4Parse.UE4.Assets.Objects
 {
@@ -12,8 +14,54 @@ namespace CUE4Parse.UE4.Assets.Objects
         ARRAY
     }
 
+    public abstract class FPropertyTagType<T> : FPropertyTagType
+    {
+        public T Value { get; protected set; }
+    }
+
     public abstract class FPropertyTagType
     {
+
+        public object? GetValue(Type type)
+        {
+            if (this is FPropertyTagType<object> prop && type == prop.Value.GetType())
+                return prop.Value;
+            else if (this is FPropertyTagType<UScriptStruct> structProp && structProp.Value.StructType.GetType() == type)
+                return structProp.Value.StructType;
+            else if (this is FPropertyTagType<UScriptArray> arrayProp && type.IsArray)
+            {
+                var array = arrayProp.Value.Properties;
+                var contentType = type.GetElementType()!;
+                var result = Array.CreateInstance(contentType, array.Count);
+                for (var i = 0; i < array.Count; i++)
+                {
+                    result.SetValue(array[i].GetValue(contentType), i);
+                }
+                return result;
+            }
+            else if (this is FPropertyTagType<FPackageIndex> objProp && typeof(UExport).IsAssignableFrom(type))
+            {
+                throw new NotImplementedException("Need to implement FileProvider first");
+            }
+            else if (this is FPropertyTagType<FSoftObjectPath> softObjProp && typeof(UExport).IsAssignableFrom(type))
+            {
+                throw new NotImplementedException("Need to implement FileProvider first");
+            }
+            else if (this is EnumProperty enumProp && type.IsEnum)
+            {
+                var storedEnum = enumProp.Value.Text;
+                if (type.Name != storedEnum.SubstringBefore("::"))
+                    return null;
+                
+                var search = storedEnum.SubstringAfter("::");
+                var values = type.GetEnumNames()!;
+                var idx = Array.FindIndex(values, it => it == search);
+                return idx == -1 ? null : type.GetEnumValues().GetValue(idx);
+            }
+            //TODO Maybe Maps?
+            return null;
+        }
+        
         internal static FPropertyTagType? ReadPropertyTagType(FAssetArchive Ar, string propertyType, FPropertyTagData? tagData, ReadType type)
         {
             switch(propertyType)
@@ -46,13 +94,6 @@ namespace CUE4Parse.UE4.Assets.Objects
                 case "NameProperty": return new NameProperty(Ar.ReadFName());
                 case "DelegateProperty": return new DelegateProperty(Ar.Read<int>(), Ar.ReadFName());
                 case "DoubleProperty": return new DoubleProperty(Ar.Read<double>());
-                case "ArrayProperty":
-                    return tagData switch
-                    {
-                        FPropertyTagData.ArrayProperty arrayProperty => new ArrayProperty(new UScriptArray(Ar, arrayProperty.InnerType.Text)),
-                        _ => null,
-                    };
-                case "StructProperty": return new StructProperty(new UScriptStruct(Ar, (tagData as FPropertyTagData.StructProperty)?.StructName.Text));
                 case "StrProperty": return new StrProperty(Ar.ReadFString());
                 case "TextProperty": return new TextProperty(new FText(Ar));
                 case "InterfaceProperty": return new InterfaceProperty(Ar.Read<UInterfaceProperty>());
@@ -67,12 +108,17 @@ namespace CUE4Parse.UE4.Assets.Objects
                 case "UInt16Property": return new UInt16Property(Ar.Read<ushort>());
                 case "Int64Property": return new Int64Property(Ar.Read<long>());
                 case "Int16Property": return new Int16Property(Ar.Read<short>());
-                case "Int8Property": return new Int8Property(Ar.Read<byte>());
-                case "MapProperty":
+                case "Int8Property": return new Int8Property(Ar.Read<sbyte>());
+                case "EnumProperty":
+                    if (type == ReadType.NORMAL && (tagData as FPropertyTagData.EnumProperty)?.EnumName.IsNone == true)
+                        return new EnumProperty(new FName());
+                    else
+                        return new EnumProperty(Ar.ReadFName());
+                case "ArrayProperty":
                     return tagData switch
                     {
-                        FPropertyTagData.MapProperty mapProperty => new MapProperty(new UScriptMap(Ar, mapProperty)),
-                        _ => null,
+                        FPropertyTagData.ArrayProperty arrayProperty => new ArrayProperty(new UScriptArray(Ar, arrayProperty.InnerType.Text)),
+                        _ => null
                     };
                 case "SetProperty":
                     return tagData switch
@@ -80,11 +126,13 @@ namespace CUE4Parse.UE4.Assets.Objects
                         FPropertyTagData.SetProperty setProperty => new SetProperty(new UScriptArray(Ar, setProperty.InnerType.Text)),
                         _ => null,
                     };
-                case "EnumProperty":
-                    if (type == ReadType.NORMAL && (tagData as FPropertyTagData.EnumProperty)?.EnumName.IsNone == true)
-                        return new EnumProperty(new FName());
-                    else
-                        return new EnumProperty(Ar.ReadFName());
+                case "StructProperty": return new StructProperty(new UScriptStruct(Ar, (tagData as FPropertyTagData.StructProperty)?.StructName.Text));
+                case "MapProperty":
+                    return tagData switch
+                    {
+                        FPropertyTagData.MapProperty mapProperty => new MapProperty(new UScriptMap(Ar, mapProperty)),
+                        _ => null,
+                    };
                 default:
 #if DEBUG
                     Console.WriteLine($"Couldn't read property type {propertyType} at {Ar.Position}");         
@@ -94,232 +142,187 @@ namespace CUE4Parse.UE4.Assets.Objects
             return null;
         }
 
-        public class BoolProperty : FPropertyTagType
+        public class BoolProperty : FPropertyTagType<bool>
         {
-            public readonly bool Value;
-
             public BoolProperty(bool value)
             {
                 Value = value;
             }
         }
         
-        public class ObjectProperty : FPropertyTagType
+        public class ObjectProperty : FPropertyTagType<FPackageIndex>
         {
-            public readonly FPackageIndex Value;
-
             public ObjectProperty(FPackageIndex value)
             {
                 Value = value;
             }
         }
         
-        public class InterfaceProperty : FPropertyTagType
+        public class InterfaceProperty : FPropertyTagType<UInterfaceProperty>
         {
-            public readonly UInterfaceProperty Value;
-
             public InterfaceProperty(UInterfaceProperty value)
             {
                 Value = value;
             }
         }
         
-        public class FloatProperty : FPropertyTagType
+        public class FloatProperty : FPropertyTagType<float>
         {
-            public readonly float Value;
-
             public FloatProperty(float value)
             {
                 Value = value;
             }
         }
 
-        public class StrProperty : FPropertyTagType
+        public class StrProperty : FPropertyTagType<string>
         {
-            public readonly string Value;
-
             public StrProperty(string value)
             {
                 Value = value;
             }
         }
         
-        public class NameProperty : FPropertyTagType
+        public class NameProperty : FPropertyTagType<FName>
         {
-            public readonly FName Value;
-
             public NameProperty(FName value)
             {
                 Value = value;
             }
         }
         
-        public class IntProperty : FPropertyTagType
+        public class IntProperty : FPropertyTagType<int>
         {
-            public readonly int Value;
-
             public IntProperty(int value)
             {
                 Value = value;
             }
         }
         
-        public class UInt16Property : FPropertyTagType
+        public class UInt16Property : FPropertyTagType<ushort>
         {
-            public readonly ushort Value;
-
             public UInt16Property(ushort value)
             {
                 Value = value;
             }
         }
         
-        public class UInt32Property : FPropertyTagType
+        public class UInt32Property : FPropertyTagType<uint>
         {
-            public readonly uint Value;
-
             public UInt32Property(uint value)
             {
                 Value = value;
             }
         }
         
-        public class UInt64Property : FPropertyTagType
+        public class UInt64Property : FPropertyTagType<ulong>
         {
-            public readonly ulong Value;
-
             public UInt64Property(ulong value)
             {
                 Value = value;
             }
         }
         
-        public class Int64Property : FPropertyTagType
+        public class Int64Property : FPropertyTagType<long>
         {
-            public readonly long Value;
-
             public Int64Property(long value)
             {
                 Value = value;
             }
         }
         
-        public class Int16Property : FPropertyTagType
+        public class Int16Property : FPropertyTagType<short>
         {
-            public readonly short Value;
-
             public Int16Property(short value)
             {
                 Value = value;
             }
         }
         
-        public class Int8Property : FPropertyTagType
+        public class Int8Property : FPropertyTagType<sbyte>
         {
-            public readonly byte Value;
-
-            public Int8Property(byte value)
+            public Int8Property(sbyte value)
             {
                 Value = value;
             }
         }
         
-        public class ByteProperty : FPropertyTagType
+        public class ByteProperty : FPropertyTagType<byte>
         {
-            public readonly byte Value;
-
             public ByteProperty(byte value)
             {
                 Value = value;
             }
         }
         
-        public class DoubleProperty : FPropertyTagType
+        public class DoubleProperty : FPropertyTagType<double>
         {
-            public readonly double Value;
-
             public DoubleProperty(double value)
             {
                 Value = value;
             }
         }
         
-        public class EnumProperty : FPropertyTagType
+        public class EnumProperty : FPropertyTagType<FName>
         {
-            public readonly FName Value;
-
             public EnumProperty(FName value)
             {
                 Value = value;
             }
         }
 
-        public class DelegateProperty : FPropertyTagType
+        public class DelegateProperty : FPropertyTagType<FName>
         {
-            public readonly int Value;
-            public readonly FName Name;
+            public readonly int Num;
 
-            public DelegateProperty(int value, FName name)
+            public DelegateProperty(int num, FName value)
             {
+                Num = num;
                 Value = value;
-                Name = name;
             }
         }
 
-        public class ArrayProperty : FPropertyTagType
+        public class ArrayProperty : FPropertyTagType<UScriptArray>
         {
-            public readonly UScriptArray Value;
-
             public ArrayProperty(UScriptArray value)
             {
                 Value = value;
             }
         }
 
-        public class StructProperty : FPropertyTagType
+        public class StructProperty : FPropertyTagType<UScriptStruct>
         {
-            public readonly UScriptStruct Value;
-
             public StructProperty(UScriptStruct value)
             {
                 Value = value;
             }
         }
 
-        public class TextProperty : FPropertyTagType
+        public class TextProperty : FPropertyTagType<FText>
         {
-            public readonly FText Value;
-
             public TextProperty(FText value)
             {
                 Value = value;
             }
         }
 
-        public class SoftObjectProperty : FPropertyTagType
+        public class SoftObjectProperty : FPropertyTagType<FSoftObjectPath>
         {
-            public readonly FSoftObjectPath Value;
-
             public SoftObjectProperty(FSoftObjectPath value)
             {
                 Value = value;
             }
         }
 
-        public class MapProperty : FPropertyTagType
+        public class MapProperty : FPropertyTagType<UScriptMap>
         {
-            public readonly UScriptMap Value;
-
             public MapProperty(UScriptMap value)
             {
                 Value = value;
             }
         }
 
-        public class SetProperty : FPropertyTagType
+        public class SetProperty : FPropertyTagType<UScriptArray>
         {
-            public readonly UScriptArray Value;
-
             public SetProperty(UScriptArray value)
             {
                 Value = value;
---
+++ CUE4Parse/Utils/StringUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CUE4Parse.Utils
+{
+    public static class StringUtils
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringBefore(this string s, char delimiter)
+        {
+            var index = s.IndexOf(delimiter);
+            return index == -1 ? s : s.Substring(0, index);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringBefore(this string s, string delimiter)
+        {
+            var index = s.IndexOf(delimiter, StringComparison.Ordinal);
+            return index == -1 ? s : s.Substring(0, index);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringAfter(this string s, char delimiter)
+        {
+            var index = s.IndexOf(delimiter);
+            return index == -1 ? s : s.Substring(index + 1, s.Length);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringAfter(this string s, string delimiter)
+        {
+            var index = s.IndexOf(delimiter, StringComparison.Ordinal);
+            return index == -1 ? s : s.Substring(index + delimiter.Length, s.Length);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringBeforeLast(this string s, char delimiter)
+        {
+            var index = s.LastIndexOf(delimiter);
+            return index == -1 ? s : s.Substring(0, index);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringBeforeLast(this string s, string delimiter)
+        {
+            var index = s.LastIndexOf(delimiter, StringComparison.Ordinal);
+            return index == -1 ? s : s.Substring(0, index);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringAfterLast(this string s, char delimiter)
+        {
+            var index = s.LastIndexOf(delimiter);
+            return index == -1 ? s : s.Substring(index + 1, s.Length);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string SubstringAfterLast(this string s, string delimiter)
+        {
+            var index = s.LastIndexOf(delimiter, StringComparison.Ordinal);
+            return index == -1 ? s : s.Substring(index + delimiter.Length, s.Length);
+        }
+    }
+}
