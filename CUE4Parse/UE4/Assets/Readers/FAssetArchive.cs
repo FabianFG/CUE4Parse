a2e1a62361acef5bfbcda6cd45183f1d7f1c4c1d
Fabian <mcdtloeser@gmail.com>
2021-02-11 20:13:38 +0100

    added missing version passthrough for FAssetArchive

--- CUE4Parse/UE4/Assets/Readers/FAssetArchive.cs
+++ CUE4Parse/UE4/Assets/Readers/FAssetArchive.cs
@@ -6,6 +6,7 @@ using CUE4Parse.UE4.Assets.Utils;
 using CUE4Parse.UE4.Exceptions;
 using CUE4Parse.UE4.Objects.UObject;
 using CUE4Parse.UE4.Readers;
+using CUE4Parse.UE4.Versions;
 
 namespace CUE4Parse.UE4.Assets.Readers
 {
@@ -20,6 +21,7 @@ namespace CUE4Parse.UE4.Assets.Readers
         private readonly Dictionary<PayloadType, Lazy<FAssetArchive?>> _payloads = new Dictionary<PayloadType, Lazy<FAssetArchive?>>();
 
         public FAssetArchive(FArchive baseArchive, IPackage owner, int absoluteOffset = 0)
+            : base(baseArchive.Ver, baseArchive.Game)
         {
             _baseArchive = baseArchive;
             Owner = owner;
