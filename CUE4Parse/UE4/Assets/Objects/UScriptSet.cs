70d417009d729260486d905eb5d10f45c78cfec7
iAmAsval <valentinbaudry64@gmail.com>
2021-05-25 19:01:51 +0200

    uscriptset

--- CUE4Parse/UE4/Assets/Objects/UScriptSet.cs
+++ CUE4Parse/UE4/Assets/Objects/UScriptSet.cs
@@ -1,58 +1,65 @@
-﻿using CUE4Parse.UE4.Assets.Readers;
-using CUE4Parse.UE4.Exceptions;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Exceptions;
 using Newtonsoft.Json;
 using Serilog;
 
 namespace CUE4Parse.UE4.Assets.Objects
 {
-    [JsonConverter(typeof(UScriptArrayConverter))]
-    public class UScriptArray
+    [JsonConverter(typeof(UScriptSetConverter))]
+    public class UScriptSet
     {
         public readonly string InnerType; 
         public readonly FPropertyTagData? InnerTagData;
         public readonly List<FPropertyTagType> Properties;
-
-        public UScriptArray(string innerType)
+        
+        public UScriptSet(string innerType)
         {
             InnerType = innerType;
             InnerTagData = null;
             Properties = new List<FPropertyTagType>();
         }
-
-        public UScriptArray(FAssetArchive Ar, FPropertyTagData? tagData)
+        
+        public UScriptSet(FAssetArchive Ar, FPropertyTagData? tagData)
         {
-            InnerType = tagData?.InnerType ?? throw new ParserException(Ar, "UScriptArray needs inner type");
-            var elementCount = Ar.Read<int>();
+            InnerType = tagData?.InnerType ?? throw new ParserException(Ar, "UScriptSet needs inner type");
+
+            var numKeyToRemove = Ar.Read<int>();
+            for (var i = 0; i < numKeyToRemove; i++)
+            {
+                FPropertyTagType.ReadPropertyTagType(Ar, tagData.InnerType, tagData, ReadType.ARRAY);
+            }
+            
             if (Ar.HasUnversionedProperties)
             {
                 InnerTagData = tagData.InnerTypeData;
             }
-            else if (InnerType == "StructProperty" || InnerType == "ArrayProperty")
+            else if (InnerType == "StructProperty" || InnerType == "SetProperty")
             {
                 InnerTagData = new FPropertyTag(Ar, false).TagData;
                 if (InnerTagData == null)
-                    throw new ParserException(Ar, $"Couldn't read ArrayProperty with inner type {InnerType}");
+                    throw new ParserException(Ar, $"Couldn't read SetProperty with inner type {InnerType}");
             }
-
-            Properties = new List<FPropertyTagType>(elementCount);
-            for (var i = 0; i < elementCount; i++)
+            
+            var numEntries = Ar.Read<int>();
+            Properties = new List<FPropertyTagType>(numEntries);
+            for (var i = 0; i < numEntries; i++)
             {
                 var property = FPropertyTagType.ReadPropertyTagType(Ar, InnerType, InnerTagData, ReadType.ARRAY);
                 if (property != null)
                     Properties.Add(property);
                 else
-                    Log.Debug($"Failed to read array property of type {InnerType} at ${Ar.Position}, index {i}");
+                    Log.Debug($"Failed to read set property of type {InnerType} at ${Ar.Position}, index {i}");
             }
         }
 
         public override string ToString() => $"{InnerType}[{Properties.Count}]";
     }
     
-    public class UScriptArrayConverter : JsonConverter<UScriptArray>
+    public class UScriptSetConverter : JsonConverter<UScriptSet>
     {
-        public override void WriteJson(JsonWriter writer, UScriptArray value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, UScriptSet value, JsonSerializer serializer)
         {
             writer.WriteStartArray();
             
@@ -64,7 +71,7 @@ namespace CUE4Parse.UE4.Assets.Objects
             writer.WriteEndArray();
         }
 
-        public override UScriptArray ReadJson(JsonReader reader, Type objectType, UScriptArray existingValue, bool hasExistingValue,
+        public override UScriptSet ReadJson(JsonReader reader, Type objectType, UScriptSet existingValue, bool hasExistingValue,
             JsonSerializer serializer)
         {
             throw new NotImplementedException();
