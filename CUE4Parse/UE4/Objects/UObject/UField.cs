6507993eaea053dfea289bb1e852e7bbf0d13a9e
Amrsatrio <satrionareswara@yahoo.com>
2021-07-03 15:49:14 +0700

    Serialize UField::Next into JSON

--- CUE4Parse/UE4/Objects/UObject/UField.cs
+++ CUE4Parse/UE4/Objects/UObject/UField.cs
@@ -1,12 +1,13 @@
 ﻿using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Versions;
+using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Objects.UObject
 {
     public class UField : Assets.Exports.UObject
     {
         /** Next Field in the linked list */
-        public FPackageIndex Next; // UField
+        public FPackageIndex? Next; // UField
 
         public override void Deserialize(FAssetArchive Ar, long validPos)
         {
@@ -14,6 +15,17 @@ namespace CUE4Parse.UE4.Objects.UObject
             if (FFrameworkObjectVersion.Get(Ar) < FFrameworkObjectVersion.Type.RemoveUField_Next)
             {
                 Next = new FPackageIndex(Ar);
+            }
+        }
+
+        protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+        {
+            base.WriteJson(writer, serializer);
+
+            if (Next != null)
+            {
+                writer.WritePropertyName("Next");
+                serializer.Serialize(writer, Next);
             }
         }
     }
