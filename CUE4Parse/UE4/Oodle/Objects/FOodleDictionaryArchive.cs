5ae59cdc61139b27fa8d17eddc325b5ac31ce824
GMatrixGames <gmatrixgames@hotmail.com>
2021-07-07 15:32:49 -0400

    UShaderBytecode support
    Still need to work out legacy versions & handling of ShaderCode

--- CUE4Parse/UE4/Oodle/Objects/FOodleDictionaryArchive.cs
+++ CUE4Parse/UE4/Oodle/Objects/FOodleDictionaryArchive.cs
@@ -6,11 +6,11 @@ namespace CUE4Parse.UE4.Oodle.Objects
     {
         private readonly int MAX_COMPRESS_BUFFER = 1024 * 1024 * 2047;
         private readonly FArchive _innerArchive;
-        
+
         public readonly FDictionaryHeader Header;
         public readonly byte[] DictionaryData;
         public readonly byte[] CompactCompresorState;
-        
+
         public FOodleDictionaryArchive(FArchive Ar)
         {
             _innerArchive = Ar;
@@ -18,12 +18,12 @@ namespace CUE4Parse.UE4.Oodle.Objects
             SerializeOodleDecompressData(Header.DictionaryData, out DictionaryData);
             SerializeOodleDecompressData(Header.CompressorData, out CompactCompresorState);
         }
-        
+
         private bool SerializeOodleDecompressData(FOodleCompressedData dataInfo, out byte[] outData)
         {
             outData = new byte[0];
-            var decompressedLength = (int)dataInfo.DecompressedLength;
-            var compressedLength = (int)dataInfo.CompressedLength;
+            var decompressedLength = (int) dataInfo.DecompressedLength;
+            var compressedLength = (int) dataInfo.CompressedLength;
             if (compressedLength > _innerArchive.Length - dataInfo.Offset || decompressedLength > MAX_COMPRESS_BUFFER ||
                 compressedLength > MAX_COMPRESS_BUFFER) return false;
 
