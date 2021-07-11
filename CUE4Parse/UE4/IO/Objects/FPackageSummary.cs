bd5decbb80007dfbe2b9e67fb7feffaaba93739f
Amrsatrio <satrionareswara@yahoo.com>
2021-07-11 16:25:23 +0700

    Basic UE5 IoPackage support

--- CUE4Parse/UE4/IO/Objects/FPackageSummary.cs
+++ CUE4Parse/UE4/IO/Objects/FPackageSummary.cs
@@ -21,4 +21,19 @@ namespace CUE4Parse.UE4.IO.Objects
         public readonly int GraphDataSize;
         private readonly int _pad;
     }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct FPackageSummary5
+    {
+        public readonly uint HeaderSize;
+        public readonly FMappedName Name;
+        public readonly FMappedName SourceName;
+        public readonly EPackageFlags PackageFlags;
+        public readonly uint CookedHeaderSize;
+        public readonly int ImportMapOffset;
+        public readonly int ExportMapOffset;
+        public readonly int ExportBundleEntriesOffset;
+        public readonly int GraphDataOffset;
+        private readonly int _pad;
+    }
 }
