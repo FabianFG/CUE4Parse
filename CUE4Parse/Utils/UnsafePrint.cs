4481900d82455f3b40f1dae7cff906223b037785
iAmAsval <valentinbaudry64@gmail.com>
2021-07-15 18:02:26 +0200

    seems to fix the crash

--- CUE4Parse/Utils/UnsafePrint.cs
+++ CUE4Parse/Utils/UnsafePrint.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace CUE4Parse.Utils
 {
@@ -21,13 +23,35 @@ namespace CUE4Parse.Utils
 
             return new string(c);
         }
-        
+
         public static unsafe string BytesToHex(params byte[] bytes)
         {
-            var result = string.Empty;
-            for (int i = 0; i < bytes.Length; i++)
+            var lookupP = _lookup32UnsafeP;
+            var result = new char[bytes.Length * 2];
+            fixed(byte* bytesP = bytes)
+            fixed (char* resultP = result)
             {
-                result += BytesToHex((byte*) Unsafe.AsPointer(ref bytes[i]), 1);
+                uint* resultP2 = (uint*)resultP;
+                for (var i = 0; i < bytes.Length; i++)
+                {
+                    resultP2[i] = lookupP[bytesP[i]];
+                }
+            }
+            return new string(result);
+        }
+        
+        private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
+        private static readonly unsafe uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe,GCHandleType.Pinned).AddrOfPinnedObject();
+        private static uint[] CreateLookup32Unsafe()
+        {
+            var result = new uint[256];
+            for (var i = 0; i < 256; i++)
+            {
+                var s = i.ToString("X2");
+                if(BitConverter.IsLittleEndian)
+                    result[i] = s[0] + ((uint)s[1] << 16);
+                else
+                    result[i] = s[1] + ((uint)s[0] << 16);
             }
             return result;
         }
