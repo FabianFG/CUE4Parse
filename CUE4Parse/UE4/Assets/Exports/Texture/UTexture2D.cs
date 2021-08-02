cef44546c34751838d89cc1b1dcfb3d8fb2462f6
MountainFlash <65584814+MinshuG@users.noreply.github.com>
2021-08-02 09:01:52 +0530

    Update UTexture2D.cs

--- CUE4Parse/UE4/Assets/Exports/Texture/UTexture2D.cs
+++ CUE4Parse/UE4/Assets/Exports/Texture/UTexture2D.cs
@@ -34,8 +34,8 @@ namespace CUE4Parse.UE4.Assets.Exports.Texture
             if (TryGetValue(out FName trigger, "LODGroup", "Filter") && !trigger.IsNone)
                 bRenderNearestNeighbor = trigger.Text.EndsWith("TEXTUREGROUP_Pixels2D", StringComparison.OrdinalIgnoreCase) ||
                                          trigger.Text.EndsWith("TF_Nearest", StringComparison.OrdinalIgnoreCase);
-            if (TryGetValue(out FName Normaltrigger, "CompressionSettings") && !Normaltrigger.IsNone)
-                isNormalMap = Normaltrigger.Text.EndsWith("TC_Normalmap", StringComparison.OrdinalIgnoreCase);
+            if (TryGetValue(out FName normalTrigger, "CompressionSettings") && !normalTrigger.IsNone)
+                isNormalMap = normalTrigger.Text.EndsWith("TC_Normalmap", StringComparison.OrdinalIgnoreCase);
 
             var stripDataFlags = Ar.Read<FStripDataFlags>();
             var bCooked = Ar.Ver >= UE4Version.VER_UE4_ADD_COOKED_TO_TEXTURE2D && Ar.ReadBoolean();
