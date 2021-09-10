73c12732922e4ad797c522895382a0b1a2c01a24
iAmAsval <valentinbaudry64@gmail.com>
2021-09-10 12:05:44 +0200

    oodle 9

--- CUE4Parse/Compression/Oodle.cs
+++ CUE4Parse/Compression/Oodle.cs
@@ -21,7 +21,7 @@ namespace CUE4Parse.Compression
         private const string WARFRAME_CDN_HOST = "https://origin.warframe.com";
         private const string WARFRAME_INDEX_PATH = "/origin/E926E926/index.txt.lzma";
         private const string WARFRAME_INDEX_URL = WARFRAME_CDN_HOST + WARFRAME_INDEX_PATH;
-        public const string OODLE_DLL_NAME = "oo2core_8_win64.dll";
+        public const string OODLE_DLL_NAME = "oo2core_9_win64.dll";
         
         public static bool LoadOodleDll()
         {
