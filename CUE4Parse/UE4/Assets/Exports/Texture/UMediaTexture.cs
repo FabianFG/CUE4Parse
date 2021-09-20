7423a772c652d5bd957dd35c67ab5f6c39db63fc
GMatrixGames <gmatrixgames@hotmail.com>
2021-09-19 22:23:35 -0400

    Add UMediaTexture to remove warnings on some assets

---
+++ CUE4Parse/UE4/Assets/Exports/Texture/UMediaTexture.cs
@@ -0,0 +1,12 @@
+using CUE4Parse.UE4.Assets.Exports.Material;
+
+namespace CUE4Parse.UE4.Assets.Exports.Texture
+{
+    public class UMediaTexture : UTexture
+    {
+        public override void GetParams(CMaterialParams parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
