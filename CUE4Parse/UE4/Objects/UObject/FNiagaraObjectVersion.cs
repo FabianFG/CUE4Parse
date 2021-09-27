7794c669db57e1c05805affa40d83c2827fb2ed4
MountainFlash <gurjar.minshu@gmail.com>
2021-09-27 13:36:34 +0530

    added FSkeletalMeshSamplingRegionBuiltData

---
+++ CUE4Parse/UE4/Objects/UObject/FNiagaraObjectVersion.cs
@@ -0,0 +1,36 @@
+﻿using CUE4Parse.UE4.Objects.Core.Misc;
+using CUE4Parse.UE4.Readers;
+using CUE4Parse.UE4.Versions;
+
+namespace CUE4Parse.UE4.Objects.UObject
+{
+    public static class FNiagaraObjectVersion
+    {
+        public enum Type
+        {
+            // Before any version changes were made
+            BeforeCustomVersionWasAdded = 0,
+
+            SkeletalMeshVertexSampling = 1,
+
+            // -----<new versions can be added above this line>-------------------------------------------------
+            VersionPlusOne,
+            LatestVersion = VersionPlusOne - 1
+        };
+
+        public static readonly FGuid GUID = new(0xF2AED0AC, 0x9AFE416F, 0x8664AA7F, 0xFA26D6FC);
+
+        public static Type Get(FArchive Ar)
+        {
+            var ver = VersionUtils.GetUE4CustomVersion(Ar, GUID);
+            if (ver >= 0)
+                return (Type) ver;
+
+            return Ar.Game switch
+            {
+                < EGame.GAME_UE4_21 => Type.BeforeCustomVersionWasAdded,
+                _ => Type.LatestVersion
+            };
+        }
+    }
+}
