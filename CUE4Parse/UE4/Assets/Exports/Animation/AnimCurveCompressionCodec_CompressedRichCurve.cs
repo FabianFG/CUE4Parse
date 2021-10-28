63e5eb4bfa85e6c92363e6275b7c3e321b8e6064
Tector <73559984+Tectors@users.noreply.github.com>
2021-10-27 17:55:01 -0700

    Update AnimCurveCompressionCodec_CompressedRichCurve.cs

--- CUE4Parse/UE4/Assets/Exports/Animation/AnimCurveCompressionCodec_CompressedRichCurve.cs
+++ CUE4Parse/UE4/Assets/Exports/Animation/AnimCurveCompressionCodec_CompressedRichCurve.cs
@@ -231,6 +231,10 @@ namespace CUE4Parse.UE4.Assets.Exports.Animation
 
         public override unsafe FFloatCurve[] ConvertCurves(UAnimSequence animSeq)
         {
+            if (animSeq.CompressedCurveByteStream == null | animSeq.CompressedCurveByteStream.Length == 0)
+            {
+                return Array.Empty<FFloatCurve>();
+            }
             fixed (byte* buffer = &animSeq.CompressedCurveByteStream[0])
             {
                 var curveDescriptions = (FCurveDesc*) buffer;
