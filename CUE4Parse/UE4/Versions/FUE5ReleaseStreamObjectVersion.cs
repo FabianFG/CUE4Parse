1022be92463f37fd8b96022a4e39680984c45f7a
MountainFlash <gurjar.minshu@gmail.com>
2022-03-23 21:52:13 +0530

    spaces

--- CUE4Parse/UE4/Versions/FUE5ReleaseStreamObjectVersion.cs
+++ CUE4Parse/UE4/Versions/FUE5ReleaseStreamObjectVersion.cs
@@ -60,55 +60,55 @@ namespace CUE4Parse.UE4.Versions
             AddLevelActorPackagingScheme,
 
             // Add support for linking to the attached parent actor in WorldPartitionActorDesc
-		    WorldPartitionActorDescSerializeAttachParent,
+            WorldPartitionActorDescSerializeAttachParent,
 
-		    // Converted AActor GridPlacement to bIsSpatiallyLoaded flag
-		    ConvertedActorGridPlacementToSpatiallyLoadedFlag,
+            // Converted AActor GridPlacement to bIsSpatiallyLoaded flag
+            ConvertedActorGridPlacementToSpatiallyLoadedFlag,
 
-		    // Fixup for bad default value for GridPlacement_DEPRECATED
-		    ActorGridPlacementDeprecateDefaultValueFixup,
+            // Fixup for bad default value for GridPlacement_DEPRECATED
+            ActorGridPlacementDeprecateDefaultValueFixup,
 
-		    // PackedLevelActor started using FWorldPartitionActorDesc (not currently checked against but added as a security)
-		    PackedLevelActorUseWorldPartitionActorDesc,
+            // PackedLevelActor started using FWorldPartitionActorDesc (not currently checked against but added as a security)
+            PackedLevelActorUseWorldPartitionActorDesc,
 
-		    // Add support for actor folder objects
-		    AddLevelActorFolders,
+            // Add support for actor folder objects
+            AddLevelActorFolders,
 
-		    // Remove FSkeletalMeshLODModel bulk datas
-		    RemoveSkeletalMeshLODModelBulkDatas,
+            // Remove FSkeletalMeshLODModel bulk datas
+            RemoveSkeletalMeshLODModelBulkDatas,
 
-		    // Exclude brightness from the EncodedHDRCubemap,
-		    ExcludeBrightnessFromEncodedHDRCubemap,
+            // Exclude brightness from the EncodedHDRCubemap,
+            ExcludeBrightnessFromEncodedHDRCubemap,
 
-		    // Unified volumetric cloud component quality sample count slider between main and reflection views for consistency
-		    VolumetricCloudSampleCountUnification,
+            // Unified volumetric cloud component quality sample count slider between main and reflection views for consistency
+            VolumetricCloudSampleCountUnification,
 
-		    // Pose asset GUID generated from source AnimationSequence
-		    PoseAssetRawDataGUID,
+            // Pose asset GUID generated from source AnimationSequence
+            PoseAssetRawDataGUID,
 
-		    // Convolution bloom now take into account FPostProcessSettings::BloomIntensity for scatter dispersion.
-		    ConvolutionBloomIntensity,
+            // Convolution bloom now take into account FPostProcessSettings::BloomIntensity for scatter dispersion.
+            ConvolutionBloomIntensity,
 
-		    // Serialize FHLODSubActors instead of FGuids in WorldPartition HLODActorDesc
-		    WorldPartitionHLODActorDescSerializeHLODSubActors,
+            // Serialize FHLODSubActors instead of FGuids in WorldPartition HLODActorDesc
+            WorldPartitionHLODActorDescSerializeHLODSubActors,
 
-		    // Large Worlds - serialize double types as doubles
-		    LargeWorldCoordinates,
+            // Large Worlds - serialize double types as doubles
+            LargeWorldCoordinates,
 
-		    // Deserialize old BP float&double types as real numbers for pins
-		    BlueprintPinsUseRealNumbers,
+            // Deserialize old BP float&double types as real numbers for pins
+            BlueprintPinsUseRealNumbers,
 
-		    // Changed shadow defaults for directional light components, version needed to not affect old things
-		    UpdatedDirectionalLightShadowDefaults,
+            // Changed shadow defaults for directional light components, version needed to not affect old things
+            UpdatedDirectionalLightShadowDefaults,
 
-		    // Refresh geometry collections that had not already generated convex bodies.
-		    GeometryCollectionConvexDefaults,
+            // Refresh geometry collections that had not already generated convex bodies.
+            GeometryCollectionConvexDefaults,
 
-		    // Add faster damping calculations to the cloth simulation and rename previous Damping parameter to LocalDamping.
-		    ChaosClothFasterDamping,
+            // Add faster damping calculations to the cloth simulation and rename previous Damping parameter to LocalDamping.
+            ChaosClothFasterDamping,
 
-		    // Serialize LandscapeActorGuid in FLandscapeActorDesc sub class.
-		    WorldPartitionLandscapeActorDescSerializeLandscapeActorGuid,
+            // Serialize LandscapeActorGuid in FLandscapeActorDesc sub class.
+            WorldPartitionLandscapeActorDescSerializeLandscapeActorGuid,
 
             // -----<new versions can be added above this line>-------------------------------------------------
             VersionPlusOne,
