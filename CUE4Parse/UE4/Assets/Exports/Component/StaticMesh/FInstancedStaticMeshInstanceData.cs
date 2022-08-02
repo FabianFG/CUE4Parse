4a54dcb16be7c2a7522a2c12a70deb695b3e35f5
floxay <57007485+floxay@users.noreply.github.com>
2022-08-02 03:13:49 +0200

    Fix SMInstanceData by using SetFromMatrix()

--- CUE4Parse/UE4/Assets/Exports/Component/StaticMesh/FInstancedStaticMeshInstanceData.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/StaticMesh/FInstancedStaticMeshInstanceData.cs
@@ -7,34 +7,13 @@ namespace CUE4Parse.UE4.Assets.Exports.Component.StaticMesh
     {
         private readonly FMatrix Transform; // don't expose the raw matrix for now
 
-        // TODO: replicate the way UE handles this data, until then this should work better than a matrix I suppose
-        public readonly FVector OffsetLocation;
-        public readonly FRotator RelativeRotation;
-        public readonly FVector RelativeScale3D;
+        public FTransform TransformData;
 
         public FInstancedStaticMeshInstanceData(FArchive Ar)
         {
             Transform = new FMatrix(Ar);
 
-            OffsetLocation = Transform.GetOrigin();
-            RelativeRotation = Transform.Rotator();
-            RelativeScale3D = Transform.GetScaleVector();
+            TransformData.SetFromMatrix(Transform);
         }
-
-        // TODO: 
-        // public FVector GetLocation()
-        // {
-        //     ...
-        // }
-
-        // public FRotator GetRotation()
-        // {
-        //     ...
-        // }
-
-        // public FVector GetScale3D()
-        // {
-        //     ...
-        // }
     }
 }
