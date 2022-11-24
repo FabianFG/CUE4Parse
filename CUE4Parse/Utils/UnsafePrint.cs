2f72ab3444479d58c8379f18c78023da0b70569e
GMatrixGames <gmatrixgames@hotmail.com>
2022-11-23 21:31:57 -0500

    Remove useless unsafe method, but we'll keep it in the class

--- CUE4Parse/Utils/UnsafePrint.cs
+++ CUE4Parse/Utils/UnsafePrint.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Runtime.InteropServices;
+using System;
 
 namespace CUE4Parse.Utils
 {
@@ -8,52 +6,24 @@ namespace CUE4Parse.Utils
     {
         public static unsafe string BytesToHex(byte* bytes, uint length)
         {
-            char[] c = new char[length * 2];
+            var c = new char[length * 2];
 
-            byte b;
-
-            for(int bx = 0, cx = 0; bx < length; ++bx, ++cx)
+            for (int bx = 0, cx = 0; bx < length; ++bx, ++cx)
             {
-                b = ((byte)(bytes[bx] >> 4));
-                c[cx] = (char)(b > 9 ? b - 10 + 'A' : b + '0');
+                var b = (byte) (bytes[bx] >> 4);
+                c[cx] = (char) (b > 9 ? b - 10 + 'A' : b + '0');
 
-                b = ((byte)(bytes[bx] & 0x0F));
-                c[++cx] = (char)(b > 9 ? b - 10 + 'A' : b + '0');
+                b = (byte) (bytes[bx] & 0x0F);
+                c[++cx] = (char) (b > 9 ? b - 10 + 'A' : b + '0');
             }
 
             return new string(c);
         }
 
-        public static unsafe string BytesToHex(params byte[] bytes)
+        public static string BytesToHex(params byte[] bytes)
         {
-            var lookupP = _lookup32UnsafeP;
-            var result = new char[bytes.Length * 2];
-            fixed(byte* bytesP = bytes)
-            fixed (char* resultP = result)
-            {
-                uint* resultP2 = (uint*)resultP;
-                for (var i = 0; i < bytes.Length; i++)
-                {
-                    resultP2[i] = lookupP[bytesP[i]];
-                }
-            }
-            return new string(result);
-        }
-        
-        private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
-        private static readonly unsafe uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe,GCHandleType.Pinned).AddrOfPinnedObject();
-        private static uint[] CreateLookup32Unsafe()
-        {
-            var result = new uint[256];
-            for (var i = 0; i < 256; i++)
-            {
-                var s = i.ToString("X2");
-                if(BitConverter.IsLittleEndian)
-                    result[i] = s[0] + ((uint)s[1] << 16);
-                else
-                    result[i] = s[1] + ((uint)s[0] << 16);
-            }
-            return result;
+            var hex = BitConverter.ToString(bytes);
+            return hex.Replace("-", "");
         }
     }
 }
