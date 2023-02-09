59bbb5cdb131acc0e2899194212c4a21605efebb
GMatrixGames <gmatrixgames@hotmail.com>
2023-02-08 21:54:48 -0500

    New option override for NavCollision

--- CUE4Parse/UE4/Assets/Exports/StaticMesh/UStaticMesh.cs
+++ CUE4Parse/UE4/Assets/Exports/StaticMesh/UStaticMesh.cs
@@ -32,7 +32,7 @@ namespace CUE4Parse.UE4.Assets.Exports.StaticMesh
             bCooked = Ar.ReadBoolean();
             BodySetup = new FPackageIndex(Ar);
 
-            if (Ar.Game != EGame.GAME_GearsOfWar4 && Ar.Game != EGame.GAME_TEKKEN7 && Ar.Ver >= EUnrealEngineObjectUE4Version.STATIC_MESH_STORE_NAV_COLLISION)
+            if (Ar.Versions["StaticMesh.HasNavCollision"])
                 NavCollision = new FPackageIndex(Ar);
 
             if (!stripDataFlags.IsEditorDataStripped())
