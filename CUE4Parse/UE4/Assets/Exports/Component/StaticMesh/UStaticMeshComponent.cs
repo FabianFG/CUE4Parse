c24bb1a031f8d463a8401e3952c9a11344e208a8
Minshu Gurjar <minshugurjar@gmail.com>
2023-02-16 06:20:26 +0530

    GetStaticMesh method

--- CUE4Parse/UE4/Assets/Exports/Component/StaticMesh/UStaticMeshComponent.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/StaticMesh/UStaticMeshComponent.cs
@@ -1,4 +1,5 @@
 using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.UObject;
 using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Assets.Exports.Component.StaticMesh
@@ -11,6 +12,21 @@ namespace CUE4Parse.UE4.Assets.Exports.Component.StaticMesh
         {
             base.Deserialize(Ar, validPos);
             LODData = Ar.ReadArray(() => new FStaticMeshComponentLODInfo(Ar));
+        }
+
+        public FPackageIndex GetStaticMesh()
+        {
+            FPackageIndex mesh = new FPackageIndex();
+            var current = this;
+            while (true)
+            {
+                mesh = current.GetOrDefault("StaticMesh", new FPackageIndex());
+                if (!mesh.IsNull || current.Template == null)
+                    break;
+                current = current.Template.Load<UStaticMeshComponent>();
+            }
+
+            return mesh;
         }
 
         protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
