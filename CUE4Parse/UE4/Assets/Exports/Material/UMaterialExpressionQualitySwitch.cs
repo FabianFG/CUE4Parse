21a7c3c4e9b205a7a50b8bc060c658e87e6bb04c
GMatrixGames <gmatrixgames@hotmail.com>
2023-03-20 20:55:02 -0400

    stupid fix for stupid problem num 2

---
+++ CUE4Parse/UE4/Assets/Exports/Material/UMaterialExpressionQualitySwitch.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Engine;
+using Newtonsoft.Json;
+
+namespace CUE4Parse.UE4.Assets.Exports.Material;
+
+public class UMaterialExpressionQualitySwitch : UMaterialExpression
+{
+    public readonly FExpressionInput[] Inputs = new FExpressionInput[(int) EMaterialQualityLevel.Num];
+    
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        var toRemove = new List<FPropertyTag>();
+
+        var i = 0;
+        foreach (var property in Properties)
+        {
+            if (property.Tag?.GenericValue is UScriptStruct { StructType: FExpressionInput input } && property.Name.Text == "Inputs")
+            {
+                Inputs[i] = input;
+                toRemove.Add(property);
+                i++;
+            }
+        }
+
+        foreach (var remove in toRemove)
+        {
+            Properties.Remove(remove);
+        }
+    }
+
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+    {
+        base.WriteJson(writer, serializer);
+
+        writer.WritePropertyName("Inputs");
+        writer.WriteStartArray();
+
+        foreach (var input in Inputs)
+        {
+            serializer.Serialize(writer, input);
+        }
+
+        writer.WriteEndArray();
+    }
+}
