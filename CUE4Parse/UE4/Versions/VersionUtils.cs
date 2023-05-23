73b6f20dfcae21bb8687390b9eda6ba8857d9dd6
LongerWarrior <LongerWarrior@gmail.com>
2023-05-23 20:54:51 +0300

    Allow Versions override in versioned assets

--- CUE4Parse/UE4/Versions/VersionUtils.cs
+++ CUE4Parse/UE4/Versions/VersionUtils.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Core.Misc;
@@ -12,17 +12,19 @@ namespace CUE4Parse.UE4.Versions
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int CustomVer(this FArchive Ar, FGuid key)
         {
+            var overrideCustomVersions = Ar.Versions.CustomVersions;
+            if (overrideCustomVersions != null)
+            {
+                var overrideCustomVersion = overrideCustomVersions.GetVersion(key);
+                if (overrideCustomVersion != -1)
+                    return overrideCustomVersion; // Return only if override
+            }
+
             var packageSummary = (Ar as FAssetArchive)?.Owner.Summary;
             if (packageSummary is { bUnversioned: false })
             {
                 var packageCustomVersion = packageSummary.CustomVersionContainer.GetVersion(key);
                 return packageCustomVersion != -1 ? packageCustomVersion : 0; // Explicitly set to BeforeCustomVersionWasAdded if not found
-            }
-
-            var overrideCustomVersions = Ar.Versions.CustomVersions;
-            if (overrideCustomVersions != null)
-            {
-                return overrideCustomVersions.GetVersion(key); // Will return determine by game if not found
             }
 
             return -1; // Determine by game
