6dde4c949fa280bc4869c7483bb55dc0b66bf6e5
halfuwu <maxman1346@gmail.com>
2023-11-27 20:06:12 -0500

    this shouldn't have been removed

--- CUE4Parse/UE4/Assets/Exports/Material/UMaterial.cs
+++ CUE4Parse/UE4/Assets/Exports/Material/UMaterial.cs
@@ -8,6 +8,7 @@ using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Engine;
 using CUE4Parse.UE4.Objects.UObject;
 using CUE4Parse.UE4.Versions;
+using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Assets.Exports.Material
 {
