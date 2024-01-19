0857407a21e11610ce770675a834f364aa39a778
Asval <asval.contactme@gmail.com>
2024-01-19 16:28:17 +0100

    FindRuntimeFormat
    https://github.com/EpicGames/UnrealEngine/blob/072300df18a94f18077ca20a14224b5d99fee872/Engine/Source/Runtime/Engine/Private/SoundWave.cpp#L182-L237

--- CUE4Parse-Conversion/Sounds/SoundDecoder.cs
+++ CUE4Parse-Conversion/Sounds/SoundDecoder.cs
@@ -5,6 +5,7 @@ using CUE4Parse.UE4.Assets.Exports.Sound;
 using CUE4Parse.UE4.Assets.Exports.Wwise;
 using CUE4Parse.UE4.Readers;
 using CUE4Parse_Conversion.Sounds.ADPCM;
+using CUE4Parse.Utils;
 
 namespace CUE4Parse_Conversion.Sounds;
 
@@ -30,7 +31,7 @@ public static class SoundDecoder
             if (soundWave.CompressedFormatData != null)
             {
                 var compressedData = soundWave.CompressedFormatData.Formats.First();
-                audioFormat = compressedData.Key.Text;
+                audioFormat = compressedData.Key.Text.SubstringBefore('_');
                 input = compressedData.Value.Data;
             }
 
