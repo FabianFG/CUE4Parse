00c37083e505d5daf2cb136a803b610c48d40fbb
Ada <ada@chronovore.dev>
2024-02-09 09:41:13 +0000

    Fix formatting

--- CUE4Parse/UE4/Readers/FChunkedArchive.cs
+++ CUE4Parse/UE4/Readers/FChunkedArchive.cs
@@ -7,42 +7,47 @@ using CUE4Parse.Utils;
 
 namespace CUE4Parse.UE4.Readers;
 
-public abstract class FChunkedArchive : FArchive {
+public abstract class FChunkedArchive : FArchive
+{
     private const int BUFFER_SIZE = 1024 * 1024 * 128; // 128 MiB.
 
-    protected FChunkedArchive(string path, VersionContainer versions) : base(versions) {
+    protected FChunkedArchive(string path, VersionContainer versions) : base(versions)
+    {
         Name = path;
         Buffer = ArrayPool<byte>.Shared.Rent(BUFFER_SIZE);
     }
 
     protected abstract Span<byte> ReadChunks(long offset, long size);
 
-    public override int Read(byte[] buffer, int offset, int count) {
-        if (Position < 0) {
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        if (Position < 0)
             return 0;
-        }
 
-        var n = (int) (Length - Position);
+        var n = (int)(Length - Position);
         if (n > count) n = count;
         if (n <= 0)
             return 0;
 
         Span<byte> data;
-        if (n < BUFFER_SIZE) {
+        if (n < BUFFER_SIZE)
+        {
             var bufferRangeStart = BufferOffset;
             var bufferRangeEnd = BufferOffset + BUFFER_SIZE;
-            if (!(bufferRangeStart <= Position && Position <= bufferRangeEnd)) {
+            if (!(bufferRangeStart <= Position && Position <= bufferRangeEnd))
+            {
                 BufferOffset = Position;
                 var blockSize = Math.Min(BUFFER_SIZE, Length - Position).Align(BUFFER_SIZE);
-                if (Position.Align(BUFFER_SIZE) != Position) {
+                if (Position.Align(BUFFER_SIZE) != Position)
                     BufferOffset = Position.Align(BUFFER_SIZE) - BUFFER_SIZE;
-                }
 
                 ReadChunks(BufferOffset, blockSize).CopyTo(Buffer);
             }
 
             data = Buffer.AsSpan().Slice((int)(Position - BufferOffset), n);
-        } else {
+        }
+        else
+        {
             data = ReadChunks(Position, n);
         }
 
@@ -55,12 +60,12 @@ public abstract class FChunkedArchive : FArchive {
     public override long Seek(long offset, SeekOrigin origin)
     {
         Position = origin switch
-                   {
-                       SeekOrigin.Begin   => offset,
-                       SeekOrigin.Current => Position + offset,
-                       SeekOrigin.End     => Length + offset,
-                       _                  => throw new ArgumentOutOfRangeException()
-                   };
+        {
+            SeekOrigin.Begin => offset,
+            SeekOrigin.Current => Position + offset,
+            SeekOrigin.End => Length + offset,
+            _ => throw new ArgumentOutOfRangeException()
+        };
         return Position;
     }
 
@@ -70,12 +75,14 @@ public abstract class FChunkedArchive : FArchive {
     private byte[] Buffer { get; }
     private long BufferOffset { get; set; } = -BUFFER_SIZE - 1;
 
-    protected void ImportBuffer(FChunkedArchive other) {
+    protected void ImportBuffer(FChunkedArchive other)
+    {
         BufferOffset = other.BufferOffset;
         other.Buffer.AsSpan().CopyTo(Buffer.AsSpan());
     }
 
-    public override void Close() {
+    public override void Close()
+    {
         base.Close();
         ArrayPool<byte>.Shared.Return(Buffer);
     }
--- CUE4Parse/UE4/Readers/FIoChunkArchive.cs
+++ CUE4Parse/UE4/Readers/FIoChunkArchive.cs
@@ -4,22 +4,28 @@ using CUE4Parse.UE4.Versions;
 
 namespace CUE4Parse.UE4.Readers;
 
-public class FIoChunkArchive : FChunkedArchive {
-    public FIoChunkArchive(string path, FIoStoreEntry entry, VersionContainer versions) : base(path, versions) {
+public class FIoChunkArchive : FChunkedArchive
+{
+    public FIoChunkArchive(string path, FIoStoreEntry entry, VersionContainer versions) : base(path, versions)
+    {
         Entry = entry;
     }
 
     public override long Length => Entry.Size;
     public FIoStoreEntry Entry { get; }
-    public override object Clone() {
-        var instance = new FIoChunkArchive(Name, Entry, Versions) {
+
+    public override object Clone()
+    {
+        var instance = new FIoChunkArchive(Name, Entry, Versions)
+        {
             Position = Position,
         };
         instance.ImportBuffer(this);
         return instance;
     }
 
-    protected override Span<byte> ReadChunks(long offset, long size) {
+    protected override Span<byte> ReadChunks(long offset, long size)
+    {
         var remaining = Math.Min(size, Entry.Size - offset);
         return remaining <= 0 ? Span<byte>.Empty : Entry.IoStoreReader.Read(offset + Entry.Offset, remaining);
     }
