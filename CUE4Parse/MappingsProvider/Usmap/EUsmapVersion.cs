f59c728dad3469c0bb015c3c0b715195f5c94391
GMatrixGames <gmatrixgames@hotmail.com>
2024-02-10 09:46:23 -0500

    name size increase in lut

--- CUE4Parse/MappingsProvider/Usmap/EUsmapVersion.cs
+++ CUE4Parse/MappingsProvider/Usmap/EUsmapVersion.cs
@@ -8,6 +8,9 @@
         /** Adds package versioning to aid with compatibility */
         PackageVersioning,
 
+        /** Increases size of names in lookup table to ushort from byte */
+        LongFName,
+
         LatestPlusOne,
         Latest = LatestPlusOne - 1
     }
