63a83ef05c6983839a1410614d916bbab07a9a28
Asval <asval.contactme@gmail.com>
2024-04-27 20:33:30 +0200

    dis easier

--- CUE4Parse/UE4/Pak/Objects/FPakCompressedBlock.cs
+++ CUE4Parse/UE4/Pak/Objects/FPakCompressedBlock.cs
@@ -8,5 +8,7 @@ namespace CUE4Parse.UE4.Pak.Objects
         public long CompressedStart;
         public long CompressedEnd;
         public long Size => CompressedEnd - CompressedStart;
+
+        public override string ToString() => $"From {CompressedStart} To {CompressedEnd} (={Size})";
     }
 }
