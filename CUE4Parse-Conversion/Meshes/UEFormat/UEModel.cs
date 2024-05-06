084b66e1cab89b0c096a145772dadce1323c6883
halfuwu <maxman1346@gmail.com>
2024-05-05 22:07:20 -0400

    remove oodle + adhere to LodFormat

--- CUE4Parse-Conversion/Meshes/UEFormat/UEModel.cs
+++ CUE4Parse-Conversion/Meshes/UEFormat/UEModel.cs
@@ -67,6 +67,8 @@ public class UEModel : UEFormatExport
                 subLodChunk.Serialize(lodChunk);
             
                 lodChunk.Count++;
+                
+                if (options.LodFormat == ELodFormat.FirstLod) break;
             }
         
             lodChunk.Serialize(Ar);
