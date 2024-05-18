3fe4665cb0d824fd54d92c980bb03488ccb234b1
GMatrixGames <gmatrixgames@hotmail.com>
2024-05-17 21:47:59 -0400

    Resolve minor issue with SoT on ULevel.

--- CUE4Parse/UE4/Objects/Engine/ULevel.cs
+++ CUE4Parse/UE4/Objects/Engine/ULevel.cs
@@ -2,6 +2,7 @@ using System.Runtime.InteropServices;
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Core.Math;
 using CUE4Parse.UE4.Objects.UObject;
+using CUE4Parse.UE4.Versions;
 using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Objects.Engine
@@ -99,6 +100,7 @@ namespace CUE4Parse.UE4.Objects.Engine
         public override void Deserialize(FAssetArchive Ar, long validPos)
         {
             base.Deserialize(Ar, validPos);
+            if (Ar.Game == EGame.GAME_SeaOfThieves) Ar.Position += 4;
             Actors = Ar.ReadArray(() => new FPackageIndex(Ar));
             URL = new FURL(Ar);
             Model = new FPackageIndex(Ar);
