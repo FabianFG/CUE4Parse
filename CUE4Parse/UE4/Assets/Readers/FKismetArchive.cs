8614a2211d1bc4383be9dcdda0cca98a46c92fb2
LongerWarrior <LongerWarrior@gmail.com>
2024-08-04 00:23:47 +0300

    Fix XFERUNICODESTRING

--- CUE4Parse/UE4/Assets/Readers/FKismetArchive.cs
+++ CUE4Parse/UE4/Assets/Readers/FKismetArchive.cs
@@ -144,9 +144,15 @@ public class FKismetArchive : FArchive
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public string XFERUNICODESTRING()
     {
-        var length = _data.AsSpan((int)Position).IndexOf(stackalloc byte[2]);
-        if (length == -1) throw new ParserException("Couldn't find end of the string");
-        if (length % 2 == 1) length++;
+        var pos = (int)Position;
+        var length = -1;
+        Span<byte> terminator = stackalloc byte[2];
+        do
+        {
+            length += _data.AsSpan(pos + length + 1).IndexOf(terminator) + 1;
+        }
+        while (length % 2 != 0 || length == -1);
+        if (length == -1) throw new ParserException("Couldn't find end of the unicode string");
         return Encoding.Unicode.GetString(ReadBytes(length));
     }
 
