a01c144292b1f941b3d67b66ff4b723d4a90d336
LongerWarrior <LongerWarrior@gmail.com>
2024-09-16 20:41:54 +0300

    FunkoFusion Support

--- CUE4Parse/UE4/Versions/EGame.cs
+++ CUE4Parse/UE4/Versions/EGame.cs
@@ -109,6 +109,7 @@ namespace CUE4Parse.UE4.Versions
             GAME_MonsterJamShowdown = GAME_UE5_3 + 4,
             GAME_Rennsport = GAME_UE5_3 + 5,
         GAME_UE5_4 = GameUtils.GameUe5Base + 4 << 4,
+            GAME_FunkoFusion = GAME_UE5_4 + 1,
         GAME_UE5_5 = GameUtils.GameUe5Base + 5 << 4,
 
         GAME_UE5_LATEST = GAME_UE5_5
