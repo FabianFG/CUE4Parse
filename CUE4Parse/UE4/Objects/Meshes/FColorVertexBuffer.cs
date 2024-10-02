be7ab5ff34138e773e3eb98a5e4b21b9bf2270c6
LongerWarrior <LongerWarrior@gmail.com>
2024-10-02 13:10:39 +0300

    Fixing Borderlands3 SM

--- CUE4Parse/UE4/Objects/Meshes/FColorVertexBuffer.cs
+++ CUE4Parse/UE4/Objects/Meshes/FColorVertexBuffer.cs
@@ -1,34 +1,37 @@
-﻿using System;
 using CUE4Parse.UE4.Objects.Core.Math;
 using CUE4Parse.UE4.Objects.Engine;
 using CUE4Parse.UE4.Readers;
 using CUE4Parse.UE4.Versions;
 using Newtonsoft.Json;
 
-namespace CUE4Parse.UE4.Objects.Meshes
+namespace CUE4Parse.UE4.Objects.Meshes;
+
+[JsonConverter(typeof(FColorVertexBufferConverter))]
+public class FColorVertexBuffer
 {
-    [JsonConverter(typeof(FColorVertexBufferConverter))]
-    public class FColorVertexBuffer
+    public readonly FColor[] Data;
+    public readonly int Stride;
+    public readonly int NumVertices;
+
+    public FColorVertexBuffer()
     {
-        public readonly FColor[] Data;
-        public readonly int Stride;
-        public readonly int NumVertices;
+        Data = [];
+    }
 
-        public FColorVertexBuffer(FArchive Ar)
+    public FColorVertexBuffer(FArchive Ar)
+    {
+        var stripDataFlags = new FStripDataFlags(Ar, FPackageFileVersion.CreateUE4Version(EUnrealEngineObjectUE4Version.STATIC_SKELETAL_MESH_SERIALIZATION_FIX));
+
+        Stride = Ar.Read<int>();
+        NumVertices = Ar.Read<int>();
+
+        if (!stripDataFlags.IsDataStrippedForServer() & NumVertices > 0)
         {
-            var stripDataFlags = new FStripDataFlags(Ar, FPackageFileVersion.CreateUE4Version(EUnrealEngineObjectUE4Version.STATIC_SKELETAL_MESH_SERIALIZATION_FIX));
-
-            Stride = Ar.Read<int>();
-            NumVertices = Ar.Read<int>();
-
-            if (!stripDataFlags.IsDataStrippedForServer() & NumVertices > 0)
-            {
-                Data = Ar.ReadBulkArray<FColor>();
-            }
-            else
-            {
-                Data = Array.Empty<FColor>();
-            }
+            Data = Ar.ReadBulkArray<FColor>();
+        }
+        else
+        {
+            Data = [];
         }
     }
 }
