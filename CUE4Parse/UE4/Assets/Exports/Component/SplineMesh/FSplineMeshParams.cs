a093fdc98fadf02a3348e4da7fe0009480aa4ea6
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2024-12-21 13:09:52 +0530

    Spline meshes

---
+++ CUE4Parse/UE4/Assets/Exports/Component/SplineMesh/FSplineMeshParams.cs
@@ -0,0 +1,24 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Objects.Core.Math;
+
+namespace CUE4Parse.UE4.Assets.Exports.Component.SplineMesh;
+
+public class FSplineMeshParams
+{
+    public FVector StartPos;
+    public FVector StartTangent;
+    public FVector2D StartOffset;
+    public FVector EndPos;
+    public FVector EndTangent;
+    public FVector2D EndOffset;
+
+    public FSplineMeshParams(FStructFallback fallback)
+    {
+        StartPos = fallback.GetOrDefault<FVector>(nameof(StartPos));
+        StartTangent = fallback.GetOrDefault<FVector>(nameof(StartTangent));
+        StartOffset = fallback.GetOrDefault<FVector2D>(nameof(StartOffset));
+        EndPos = fallback.GetOrDefault<FVector>(nameof(EndPos));
+        EndTangent = fallback.GetOrDefault<FVector>(nameof(EndTangent));
+        EndOffset = fallback.GetOrDefault<FVector2D>(nameof(EndOffset));
+    }
+}
---
+++ CUE4Parse/UE4/Assets/Exports/Component/SplineMesh/USplineMeshComponent.cs
@@ -0,0 +1,29 @@
+﻿using CUE4Parse.UE4.Assets.Exports.Component.StaticMesh;
+using CUE4Parse.UE4.Assets.Exports.Material;
+using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Core.Misc;
+using CUE4Parse.UE4.Objects.UObject;
+
+namespace CUE4Parse.UE4.Assets.Exports.Component.SplineMesh;
+
+public class USplineMeshComponent : UStaticMeshComponent
+{
+    public FSplineMeshParams SplineParams;
+    public FGuid CachedMeshBodySetupGuid;
+    public FPackageIndex? StaticMesh;
+    public FPackageIndex[] OverrideMaterials;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        CachedMeshBodySetupGuid = GetOrDefault<FGuid>(nameof(CachedMeshBodySetupGuid));
+        StaticMesh = GetOrDefault<FPackageIndex>(nameof(StaticMesh));
+
+        if (TryGetValue(out FStructFallback fallback, nameof(SplineParams)))
+            SplineParams = new FSplineMeshParams(fallback);
+
+        OverrideMaterials = GetOrDefault<FPackageIndex[]>(nameof(OverrideMaterials), []);
+    }
+}
