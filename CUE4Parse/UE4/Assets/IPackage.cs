fa905650af5e18caeeeecb4e813005aaf49ba96b
Asval <asval.contactme@gmail.com>
2025-01-19 02:10:12 +0100

    AbstractPropertyHolder

--- CUE4Parse/UE4/Assets/IPackage.cs
+++ CUE4Parse/UE4/Assets/IPackage.cs
@@ -5,31 +5,29 @@ using CUE4Parse.MappingsProvider;
 using CUE4Parse.UE4.Assets.Exports;
 using CUE4Parse.UE4.Objects.UObject;
 
-namespace CUE4Parse.UE4.Assets
+namespace CUE4Parse.UE4.Assets;
+
+public interface IPackage
 {
+    public string Name { get; set; }
+    public IFileProvider? Provider { get; }
+    public TypeMappings? Mappings { get; }
 
-    public interface IPackage
-    {
-        public string Name { get; set; }
-        public IFileProvider? Provider { get; }
-        public TypeMappings? Mappings { get; }
+    public FPackageFileSummary Summary { get; }
+    public FNameEntrySerialized[] NameMap { get; }
+    public Lazy<UObject>[] ExportsLazy { get; }
 
-        public FPackageFileSummary Summary { get; }
-        public FNameEntrySerialized[] NameMap { get; }
-        public Lazy<UObject>[] ExportsLazy { get; }
+    public abstract bool IsFullyLoaded { get; }
 
-        public abstract bool IsFullyLoaded { get; }
-
-        public bool HasFlags(EPackageFlags flags);
-        /*public T? GetExportOfTypeOrNull<T>() where T : UObject;
-        public T GetExportOfType<T>() where T : UObject;*/
-        public UObject? GetExportOrNull(string name, StringComparison comparisonType = StringComparison.Ordinal);
-        public T? GetExportOrNull<T>(string name, StringComparison comparisonType = StringComparison.Ordinal) where T : UObject;
-        public UObject GetExport(string name, StringComparison comparisonType = StringComparison.Ordinal);
-        public T GetExport<T>(string name, StringComparison comparisonType = StringComparison.Ordinal) where T : UObject;
-        public Lazy<UObject>? FindObject(FPackageIndex? index);
-        public ResolvedObject? ResolvePackageIndex(FPackageIndex? index);
-        public UObject? GetExport(int index);
-        public IEnumerable<UObject> GetExports();
-    }
+    public bool HasFlags(EPackageFlags flags);
+    /*public T? GetExportOfTypeOrNull<T>() where T : UObject;
+    public T GetExportOfType<T>() where T : UObject;*/
+    public UObject? GetExportOrNull(string name, StringComparison comparisonType = StringComparison.Ordinal);
+    public T? GetExportOrNull<T>(string name, StringComparison comparisonType = StringComparison.Ordinal) where T : UObject;
+    public UObject GetExport(string name, StringComparison comparisonType = StringComparison.Ordinal);
+    public T GetExport<T>(string name, StringComparison comparisonType = StringComparison.Ordinal) where T : UObject;
+    public Lazy<UObject>? FindObject(FPackageIndex? index);
+    public ResolvedObject? ResolvePackageIndex(FPackageIndex? index);
+    public UObject? GetExport(int index);
+    public IEnumerable<UObject> GetExports();
 }
