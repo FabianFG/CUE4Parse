6e13883a127500e4a3798cf79cf216466a8764b7
Asval <asval.contactme@gmail.com>
2025-02-18 18:52:13 +0100

    more wwise shit

---
+++ CUE4Parse/UE4/Assets/Exports/Wwise/UAkAudioDeviceShareSet.cs
@@ -0,0 +1,25 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+using Newtonsoft.Json;
+
+namespace CUE4Parse.UE4.Assets.Exports.Wwise;
+
+public class UAkAudioDeviceShareSet : UAkAudioType
+{
+    public FStructFallback AudioDeviceShareSetCookedData { get; private set; }
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        AudioDeviceShareSetCookedData = new FStructFallback(Ar, "WwiseAudioDeviceShareSetCookedData");
+    }
+
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+    {
+        base.WriteJson(writer, serializer);
+
+        writer.WritePropertyName("AudioDeviceShareSetCookedData");
+        serializer.Serialize(writer, AudioDeviceShareSetCookedData);
+    }
+}
