eb9c1a2092e438913572c27547c1c3c988ca3c88
Asval <asval.contactme@gmail.com>
2025-02-18 18:30:28 +0100

    UAkAudioEvent support

---
+++ CUE4Parse/UE4/Assets/Exports/Wwise/UAkAudioEvent.cs
@@ -0,0 +1,45 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+using Newtonsoft.Json;
+
+namespace CUE4Parse.UE4.Assets.Exports.Wwise;
+
+public class UAkAudioEvent : UAkAudioType
+{
+    public FStructFallback EventCookedData { get; private set; }
+    public float MaximumDuration { get; private set; }
+    public float MinimumDuration { get; private set; }
+    public bool IsInfinite { get; private set; }
+    public float MaxAttenuationRadius { get; private set; }
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        EventCookedData = new FStructFallback(Ar, "WwiseLocalizedEventCookedData");
+        MaximumDuration = Ar.Read<float>();
+        MinimumDuration = Ar.Read<float>();
+        IsInfinite = Ar.ReadBoolean();
+        MaxAttenuationRadius = Ar.Read<float>();
+    }
+
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+    {
+        base.WriteJson(writer, serializer);
+
+        writer.WritePropertyName("EventCookedData");
+        serializer.Serialize(writer, EventCookedData);
+
+        writer.WritePropertyName("MaximumDuration");
+        writer.WriteValue(MaximumDuration);
+
+        writer.WritePropertyName("MinimumDuration");
+        writer.WriteValue(MinimumDuration);
+
+        writer.WritePropertyName("IsInfinite");
+        writer.WriteValue(IsInfinite);
+
+        writer.WritePropertyName("MaxAttenuationRadius");
+        writer.WriteValue(MaxAttenuationRadius);
+    }
+}
---
+++ CUE4Parse/UE4/Assets/Exports/Wwise/UAkAudioType.cs
@@ -0,0 +1,11 @@
+﻿using CUE4Parse.UE4.Assets.Readers;
+
+namespace CUE4Parse.UE4.Assets.Exports.Wwise;
+
+public class UAkAudioType : UObject
+{
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+    }
+}
