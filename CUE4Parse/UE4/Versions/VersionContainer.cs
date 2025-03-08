77906a29a988baa1a68ddd1fadbcaf5c9799a70d
LongerWarrior <LongerWarrior@gmail.com>
2025-03-08 19:50:34 +0200

    Adding SplitFiction and Stray versions, and some FF7R fixes

--- CUE4Parse/UE4/Versions/VersionContainer.cs
+++ CUE4Parse/UE4/Versions/VersionContainer.cs
@@ -86,7 +86,7 @@ namespace CUE4Parse.UE4.Versions
             Options["StaticMesh.HasVisibleInRayTracing"] = Game >= GAME_UE4_26;
             Options["StaticMesh.UseNewCookedFormat"] = Game >= GAME_UE4_23;
             Options["VirtualTextures"] = Game >= GAME_UE4_23;
-            Options["SoundWave.UseAudioStreaming"] = Game >= GAME_UE4_25 && Game != GAME_UE4_28 && Game != GAME_GTATheTrilogyDefinitiveEdition && Game != GAME_ReadyOrNot && Game != GAME_BladeAndSoul; // A lot of games use this, but some don't, which causes issues.
+            Options["SoundWave.UseAudioStreaming"] = Game >= GAME_UE4_25 && OverrideUseAudioStreaming(); // A lot of games use this, but some don't, which causes issues.
             Options["AnimSequence.HasCompressedRawSize"] = Game >= GAME_UE4_17; // Early 4.17 builds don't have this, and some custom engine builds don't either.
             Options["StaticMesh.HasNavCollision"] = Ver >= EUnrealEngineObjectUE4Version.STATIC_MESH_STORE_NAV_COLLISION && Game != GAME_GearsOfWar4 && Game != GAME_TEKKEN7;
 
@@ -105,6 +105,11 @@ namespace CUE4Parse.UE4.Versions
             {
                 Options[key] = value;
             }
+        }
+
+        private bool OverrideUseAudioStreaming()
+        {
+            return Game is not (GAME_UE4_28 or GAME_GTATheTrilogyDefinitiveEdition or GAME_ReadyOrNot or GAME_BladeAndSoul or GAME_Stray);
         }
 
         private void InitMapStructTypes()
