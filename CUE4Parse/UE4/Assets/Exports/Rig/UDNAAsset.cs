4968b95ab6088c702ee87007f3b16c76277f34b4
Asval <asval.contactme@gmail.com>
2025-04-21 18:19:14 +0200

    missing using

--- CUE4Parse/UE4/Assets/Exports/Rig/UDNAAsset.cs
+++ CUE4Parse/UE4/Assets/Exports/Rig/UDNAAsset.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Readers;
 using CUE4Parse.UE4.Versions;
