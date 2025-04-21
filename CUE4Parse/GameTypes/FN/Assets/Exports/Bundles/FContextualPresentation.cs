f2e8d70f3a1bbc473a21b5c12abe5464cad7034c
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2025-04-21 21:43:39 +0530

    oof

---
+++ CUE4Parse/GameTypes/FN/Assets/Exports/Bundles/FContextualPresentation.cs
@@ -0,0 +1,21 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Utils;
+using CUE4Parse.UE4.Objects.GameplayTags;
+using CUE4Parse.UE4.Objects.UObject;
+
+namespace CUE4Parse.GameTypes.FN.Assets.Exports.Bundles;
+
+[StructFallback]
+public class FContextualPresentation
+{
+    public FGameplayTag ProductTag;
+    public FSoftObjectPath RenderImage;
+    public FSoftObjectPath OverrideImageMaterial;
+
+    public FContextualPresentation(FStructFallback fallback)
+    {
+        ProductTag = fallback.GetOrDefault<FGameplayTag>(nameof(ProductTag));
+        RenderImage = fallback.GetOrDefault<FSoftObjectPath>(nameof(RenderImage));
+        OverrideImageMaterial = fallback.GetOrDefault<FSoftObjectPath>(nameof(OverrideImageMaterial));
+    }
+}
---
+++ CUE4Parse/GameTypes/FN/Assets/Exports/Bundles/UAthenaItemShopOfferDisplayData.cs
@@ -0,0 +1,16 @@
+﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Engine;
+
+namespace CUE4Parse.GameTypes.FN.Assets.Exports.Bundles;
+
+public class UAthenaItemShopOfferDisplayData : UPrimaryDataAsset
+{
+    public FContextualPresentation[] ContextualPresentations;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        ContextualPresentations = GetOrDefault<FContextualPresentation[]>(nameof(ContextualPresentations), []);
+    }
+}
