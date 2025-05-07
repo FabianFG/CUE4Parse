a4ea2637e7b1ce7f5d42ade2ada1dfa968084962
LongerWarrior <LongerWarrior@gmail.com>
2025-05-07 23:52:09 +0300

    Fixes

--- CUE4Parse/UE4/Assets/Exports/WorldPartition/UWorldPartitionRuntimeCellData.cs
+++ CUE4Parse/UE4/Assets/Exports/WorldPartition/UWorldPartitionRuntimeCellData.cs
@@ -1,17 +1,18 @@
-﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Versions;
 
 namespace CUE4Parse.UE4.Assets.Exports.WorldPartition;
 
 public class UWorldPartitionRuntimeCellData : UObject
 {
-    public string DebugName;
+    public string? DebugName;
 
     public override void Deserialize(FAssetArchive Ar, long validPos)
     {
         base.Deserialize(Ar, validPos);
 
-        DebugName = Ar.ReadFString();
+        DebugName = Ar.Game >= EGame.GAME_UE5_3 ? Ar.ReadFString() : null;
     }
 }
 
-public class UWorldPartitionRuntimeCell : UObject { }
+public class UWorldPartitionRuntimeCell : UObject;
--- CUE4Parse/UE4/Objects/Engine/Animation/FSmartName.cs
+++ CUE4Parse/UE4/Objects/Engine/Animation/FSmartName.cs
@@ -1,34 +1,33 @@
-﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Core.Misc;
 using CUE4Parse.UE4.Objects.UObject;
-using CUE4Parse.UE4.Readers;
 using CUE4Parse.UE4.Versions;
 
-namespace CUE4Parse.UE4.Objects.Engine.Animation
+namespace CUE4Parse.UE4.Objects.Engine.Animation;
+
+//[JsonConverter(typeof(FSmartNameConverter))] For consistency with the property serialization structure
+public readonly struct FSmartName : IUStruct
 {
-    //[JsonConverter(typeof(FSmartNameConverter))] For consistency with the property serialization structure
-    public readonly struct FSmartName : IUStruct
+    public readonly FName DisplayName;
+
+    public FSmartName(FAssetArchive Ar)
     {
-        public readonly FName DisplayName;
-
-        public FSmartName(FArchive Ar)
+        DisplayName = Ar.ReadFName();
+        if (FAnimPhysObjectVersion.Get(Ar) < FAnimPhysObjectVersion.Type.RemoveUIDFromSmartNameSerialize)
         {
-            DisplayName = Ar.ReadFName();
-            if (FAnimPhysObjectVersion.Get(Ar) < FAnimPhysObjectVersion.Type.RemoveUIDFromSmartNameSerialize)
-            {
-                Ar.Read<ushort>(); // TempUID
-            }
-
-            // only save if it's editor build and not cooking
-            if (FAnimPhysObjectVersion.Get(Ar) < FAnimPhysObjectVersion.Type.SmartNameRefactorForDeterministicCooking)
-            {
-                Ar.Read<FGuid>(); // TempGUID
-            }
+            Ar.Read<ushort>(); // TempUID
         }
 
-        public FSmartName(FStructFallback data)
+        // only save if it's editor build and not cooking
+        if (!Ar.IsFilterEditorOnly && FAnimPhysObjectVersion.Get(Ar) < FAnimPhysObjectVersion.Type.SmartNameRefactorForDeterministicCooking)
         {
-            DisplayName = data.GetOrDefault<FName>(nameof(DisplayName));
+            Ar.Read<FGuid>(); // TempGUID
         }
+    }
+
+    public FSmartName(FStructFallback data)
+    {
+        DisplayName = data.GetOrDefault<FName>(nameof(DisplayName));
     }
 }
