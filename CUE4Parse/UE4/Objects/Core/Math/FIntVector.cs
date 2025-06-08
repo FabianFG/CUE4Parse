4aac99e16ce32364f72662076d50493bc712a5d9
Guillaume Mercier <C0bra5@users.noreply.github.com>
2025-06-08 15:54:00 -0400

    Nanite Parsing Fixes (#215)
    * Nanite Fixes
    
    I'm currently working on reverse-engineering nanite so I can extract high quality meshes that use it. This library clearly has some issues reading the nanite data from the oblivion remaster and well I can see that the data that CUE4/FModel outputs is clearly gibberish when it comes to parsing the root page.
    
    I'm only working with 5.3.2 but I did check to make sure anything that is gated after 5.3 should be functional. I don't have a way to test assets from versions prior to 5.3 right now, so I'm going to have it use the old logic to backwards compatibility.
    
    I'll probably end up making an other PR once I've got a better solution for extracting all the nanite data. Right now I just need to figure out how distinguish lowest level LODs and I'll be pretty much done I think™
    
    * fix reading of float16s
    
    * Add complete parsing for nanite pages for UE 5.3
    
    This adds full support for parsing nanite stream pages. Export to files to come in the near future hopefully.
    
    * bug fixes
    
    * cleanup nanite parsing
    
    * add nanite mesh conversion
    
    * cleanup FPackedHierarchyNode
    
    * fix uvs hopefully
    
    honestly just shooting in the dark hoping to hit gold here
    
    * 5.2 support
    
    Cluster navigation made by @LongerWarrior
    
    Co-Authored-By: LongerWarrior <37636768+LongerWarrior@users.noreply.github.com>
    
    * bugfix
    
    * 5.0 and 5.1 support
    
    @LongerWarrior found the more effective method of finding high quality clusters, and it seems to work on all 5.2 and below assets.
    
    Co-Authored-By: LongerWarrior <37636768+LongerWarrior@users.noreply.github.com>
    
    * code cleanup
    
    * bugfix
    
    * partial 5.4 compatibility
    
    ---------
    
    Co-authored-by: LongerWarrior <37636768+LongerWarrior@users.noreply.github.com>

--- CUE4Parse/UE4/Objects/Core/Math/FIntVector.cs
+++ CUE4Parse/UE4/Objects/Core/Math/FIntVector.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace CUE4Parse.UE4.Objects.Core.Math
 {
@@ -19,5 +20,42 @@ namespace CUE4Parse.UE4.Objects.Core.Math
         }
 
         public override string ToString() => $"{nameof(X)}: {X}, {nameof(Y)}: {Y}, {nameof(Z)}: {Z}";
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator +(FIntVector a, FIntVector b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator +(FIntVector a, int bias) => new(a.X + bias, a.Y + bias, a.Z + bias);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator +(FIntVector a, FVector b) => new FVector(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator +(FIntVector a, float bias) => new FVector(a.X + bias, a.Y + bias, a.Z + bias);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator -(FIntVector a, FIntVector b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator -(FIntVector a, int bias) => new(a.X - bias, a.Y - bias, a.Z - bias);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator -(FIntVector a, FVector b) => new FVector(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator -(FIntVector a, float bias) => new FVector(a.X - bias, a.Y - bias, a.Z - bias);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator *(FIntVector a, FIntVector b) => new(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator *(FIntVector a, int bias) => new(a.X * bias, a.Y * bias, a.Z * bias);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator *(FIntVector a, FVector b) => new FVector(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator *(FIntVector a, float bias) => new FVector(a.X * bias, a.Y * bias, a.Z * bias);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator /(FIntVector a, FIntVector b) => new(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FIntVector operator /(FIntVector a, int bias) => new(a.X / bias, a.Y / bias, a.Z / bias);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator /(FIntVector a, FVector b) => new FVector(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FVector operator /(FIntVector a, float bias) => new FVector(a.X / bias, a.Y / bias, a.Z / bias);
+
     }
 }
