7b9f450e41c114cb7ab98f5a31fda3a1636cec11
GMatrixGames <gmatrixgames@hotmail.com>
2025-06-07 22:03:59 -0400

    GGS binary object

---
+++ CUE4Parse/GameTypes/RED/Assets/Exports/REDBinaryObject.cs
@@ -0,0 +1,31 @@
+﻿using CUE4Parse.UE4.Assets.Exports;
+using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+using Newtonsoft.Json;
+
+namespace CUE4Parse.GameTypes.RED.Assets.Exports;
+
+public class UREDBinaryObject : UObject
+{
+    public FByteBulkData DataBE;
+    public FByteBulkData DataLE;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        DataBE = new FByteBulkData(Ar);
+        DataLE = new FByteBulkData(Ar);
+    }
+
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+    {
+        base.WriteJson(writer, serializer);
+
+        writer.WritePropertyName("DataBE");
+        serializer.Serialize(writer, DataBE);
+
+        writer.WritePropertyName("DataLE");
+        serializer.Serialize(writer, DataLE);
+    }
+}
