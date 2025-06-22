181fca0934da5f2a36ee117ec98124f7407761ae
GMatrixGames <gmatrixgames@hotmail.com>
2025-06-22 16:12:57 -0400

    Add UAkMediaAsset unreal type

--- CUE4Parse/UE4/Assets/Exports/Wwise/UAkAudioType.cs
+++ CUE4Parse/UE4/Assets/Exports/Wwise/UAkAudioType.cs
@@ -1,11 +1,3 @@
-﻿using CUE4Parse.UE4.Assets.Readers;
+﻿namespace CUE4Parse.UE4.Assets.Exports.Wwise;
 
-namespace CUE4Parse.UE4.Assets.Exports.Wwise;
-
-public class UAkAudioType : UObject
-{
-    public override void Deserialize(FAssetArchive Ar, long validPos)
-    {
-        base.Deserialize(Ar, validPos);
-    }
-}
+public class UAkAudioType : UObject;
