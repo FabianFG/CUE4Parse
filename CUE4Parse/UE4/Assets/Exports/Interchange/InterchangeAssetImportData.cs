b6c37629edb6be72b01540203ea3b6f776227982
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2025-07-31 09:12:04 +1000

    Interchange

--- CUE4Parse/UE4/Assets/Exports/Interchange/InterchangeAssetImportData.cs
+++ CUE4Parse/UE4/Assets/Exports/Interchange/InterchangeAssetImportData.cs
@@ -1,31 +1,44 @@
 ﻿using CUE4Parse.UE4.Assets.Readers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using CUE4Parse.UE4.Versions;
+using Newtonsoft.Json.Linq;
 
 namespace CUE4Parse.UE4.Assets.Exports.Interchange;
 
-public class AssetImportData : UObject
+public class InterchangeAssetImportData : AssetImportData
 {
-    public string CachedNodeContainer;
-    public string CachedPipelines;
+    public byte[] CachedNodeContainer = [];
+    public List<KeyValuePair<string, JToken>> CachedPipelines = [];
+
     public override void Deserialize(FAssetArchive Ar, long validPos)
     {
-        // it tries to read a FString but PropertyTag is FName???? 
-        //base.Deserialize(Ar, validPos);
-        CachedNodeContainer = Ar.ReadString();
-        CachedPipelines = Ar.ReadString();
+        base.Deserialize(Ar, validPos);
+
+        if (FInterchangeCustomVersion.Get(Ar) >= FInterchangeCustomVersion.Type.SerializedInterchangeObjectStoring)
+        {
+            long count = Ar.Read<long>();
+            CachedNodeContainer = Ar.ReadBytes((int)count);
+
+            int numPipelines = Ar.Read<int>();
+            for (int i = 0; i < numPipelines; i++)
+            {
+                var key = Ar.ReadFString();
+                var value = Ar.ReadFString();
+                JToken jsonObj = JToken.Parse(value);
+                CachedPipelines.Add(new KeyValuePair<string, JToken>(key, jsonObj));
+            }
+        }
     }
 
-    protected internal override void WriteJson(Newtonsoft.Json.JsonWriter writer, Newtonsoft.Json.JsonSerializer serializer)
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
     {
         base.WriteJson(writer, serializer);
-        if (CachedNodeContainer is not null)
-        {
-            writer.WritePropertyName("CachedNodeContainer");
-            serializer.Serialize(writer, CachedNodeContainer);
-        }
-        if (CachedPipelines is not null)
-        {
-            writer.WritePropertyName("CachedPipelines");
-            serializer.Serialize(writer, CachedPipelines);
-        }
+        
+        writer.WritePropertyName("CachedNodeContainer");
+        writer.WriteValue(CachedNodeContainer);
+
+        writer.WritePropertyName("CachedPipelines");
+        serializer.Serialize(writer, CachedPipelines);
     }
 }
