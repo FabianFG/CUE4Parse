5f74b32fe22fe8b5255476fc837bf851b2c397ff
Asval <asval.contactme@gmail.com>
2025-08-06 17:54:54 +0200

    more world partition stuff

---
+++ CUE4Parse/UE4/Assets/Exports/WorldPartition/UWorldPartitionLevelStreamingDynamic.cs
@@ -0,0 +1,18 @@
+﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Engine;
+using CUE4Parse.UE4.Objects.UObject;
+
+namespace CUE4Parse.UE4.Assets.Exports.WorldPartition;
+
+public class UWorldPartitionLevelStreamingDynamic : ULevelStreamingDynamic
+{
+    public FPackageIndex? StreamingCell;
+    public FPackageIndex? OuterWorldPartition;
+    
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+        StreamingCell = GetOrDefault<FPackageIndex>(nameof(StreamingCell));
+        OuterWorldPartition = GetOrDefault<FPackageIndex>(nameof(OuterWorldPartition));
+    }
+}
---
+++ CUE4Parse/UE4/Objects/Engine/ULevelStreaming.cs
@@ -0,0 +1,17 @@
+﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.UObject;
+
+namespace CUE4Parse.UE4.Objects.Engine;
+
+public class ULevelStreaming : Assets.Exports.UObject
+{
+    public FSoftObjectPath? WorldAsset;
+    
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+        WorldAsset = GetOrDefault<FSoftObjectPath>(nameof(WorldAsset));
+    }
+}
+
+public class ULevelStreamingDynamic : ULevelStreaming;
