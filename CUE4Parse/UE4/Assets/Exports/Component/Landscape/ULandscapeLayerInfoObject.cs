ce22b4a6da93ea8774f8dab5e0d2ba96cb7a2c6f
Asval <asval.contactme@gmail.com>
2025-08-13 19:07:27 +0200

    LayerName is FName

--- CUE4Parse/UE4/Assets/Exports/Component/Landscape/ULandscapeLayerInfoObject.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/Landscape/ULandscapeLayerInfoObject.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CUE4Parse.UE4.Assets.Readers;
+﻿using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Core.Math;
 using CUE4Parse.UE4.Objects.UObject;
 
@@ -7,14 +6,15 @@ namespace CUE4Parse.UE4.Assets.Exports.Component.Landscape;
 
 public class ULandscapeLayerInfoObject : UObject {
     
-    public string LayerName;
+    public FName LayerName;
     public FLinearColor LayerUsageDebugColor;
     public FPackageIndex PhysMaterial;
     
-    public override void Deserialize(FAssetArchive Ar, long validPos) {
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
         base.Deserialize(Ar, validPos);
 
-        LayerName = GetOrDefault(nameof(LayerName), String.Empty);
+        LayerName = GetOrDefault<FName>(nameof(LayerName));
         LayerUsageDebugColor = GetOrDefault(nameof(LayerUsageDebugColor), new FLinearColor());
         PhysMaterial = GetOrDefault<FPackageIndex>(nameof(PhysMaterial));
     }
