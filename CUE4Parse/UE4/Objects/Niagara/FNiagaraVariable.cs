f6725d28e0c74482738a6031a945f53a45c738c2
LongerWarrior <LongerWarrior@gmail.com>
2025-08-27 01:26:50 +0300

    Hell let Loose fix

--- CUE4Parse/UE4/Objects/Niagara/FNiagaraVariable.cs
+++ CUE4Parse/UE4/Objects/Niagara/FNiagaraVariable.cs
@@ -1,6 +1,7 @@
 using CUE4Parse.UE4.Assets.Objects;
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.UObject;
+using CUE4Parse.UE4.Versions;
 
 namespace CUE4Parse.UE4.Objects.Niagara;
 
@@ -10,9 +11,15 @@ public class FNiagaraVariable : FNiagaraVariableBase
 
     public FNiagaraVariable(FAssetArchive Ar) : base(Ar)
     {
-        if (Ar.Game == Versions.EGame.GAME_FinalFantasy7Rebirth) Ar.Position += 8;
+        if (Ar.Game == EGame.GAME_FinalFantasy7Rebirth) Ar.Position += 8;
+        if (Ar.Game == EGame.GAME_HellLetLoose && FallbackStruct is not null)
+        {
+            VarData =  FallbackStruct.GetOrDefault<byte[]>(nameof(VarData));
+            return;
+        }
+
         VarData = Ar.ReadArray<byte>();
-        if (Ar.Game == Versions.EGame.GAME_FinalFantasy7Rebirth) Ar.Position += 4;
+        if (Ar.Game == EGame.GAME_FinalFantasy7Rebirth) Ar.Position += 4;
     }
 
     public FNiagaraVariable(FName name, FStructFallback typeDef, byte[] varData) : base(name, typeDef)
