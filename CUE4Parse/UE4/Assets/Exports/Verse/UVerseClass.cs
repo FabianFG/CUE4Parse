9d328c4cbfed11d85560ef3068a3f829f131af02
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2025-09-21 23:30:45 +1000

    New LocMeta and verse struct fix (ghost)

--- CUE4Parse/UE4/Assets/Exports/Verse/UVerseClass.cs
+++ CUE4Parse/UE4/Assets/Exports/Verse/UVerseClass.cs
@@ -5,5 +5,4 @@ namespace CUE4Parse.UE4.Assets.Exports.Verse;
 public class UVerseVMClass : UClass;
 public class UVerseClassBase : UVerseVMClass;
 public class UVerseClass : UVerseClassBase;
-public class UVerseStruct : UScriptStruct;
 public class UVerseEnum : UEnum;
