aa9ce2eb0e0dc9dee5f1e848c78d811246818c55
Asval <asval.contactme@gmail.com>
2025-10-09 21:22:36 +0200

    AttenuationSettings test

---
+++ CUE4Parse/UE4/Assets/Exports/Sound/FBaseAttenuationSettings.cs
@@ -0,0 +1,15 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Utils;
+
+namespace CUE4Parse.UE4.Assets.Exports.Sound;
+
+[StructFallback]
+public class FBaseAttenuationSettings
+{
+    public readonly float FalloffDistance;
+    
+    public FBaseAttenuationSettings(FStructFallback fallback)
+    {
+        FalloffDistance = fallback.GetOrDefault<float>(nameof(FalloffDistance));
+    }
+}
---
+++ CUE4Parse/UE4/Assets/Exports/Sound/FSoundAttenuationSettings.cs
@@ -0,0 +1,13 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Utils;
+
+namespace CUE4Parse.UE4.Assets.Exports.Sound;
+
+[StructFallback]
+public class FSoundAttenuationSettings : FBaseAttenuationSettings
+{
+    public FSoundAttenuationSettings(FStructFallback fallback) : base(fallback)
+    {
+        
+    }
+}
