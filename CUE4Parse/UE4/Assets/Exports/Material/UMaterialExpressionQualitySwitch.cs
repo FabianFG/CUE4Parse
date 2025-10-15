668694ebc112446d63ea094102052552b62e89a3
Tector <73559984+Tectors@users.noreply.github.com>
2025-10-15 07:13:25 -0700

    Remove WriteJson for FeatureLevelSwitch/QualitySwitch, Write to Inputs array properly (#251)
    * Remove WriteJson for FeatureLevelSwitch/QualitySwitch, Write to Inputs[] array properly
    
    * Use TryGetAllValues

--- CUE4Parse/UE4/Assets/Exports/Material/UMaterialExpressionQualitySwitch.cs
+++ CUE4Parse/UE4/Assets/Exports/Material/UMaterialExpressionQualitySwitch.cs
@@ -1,48 +1,18 @@
-using System.Collections.Generic;
-using CUE4Parse.UE4.Assets.Objects;
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.Engine;
-using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Assets.Exports.Material;
 
 public class UMaterialExpressionQualitySwitch : UMaterialExpression
 {
-    public List<FExpressionInput> Inputs = new((int) EMaterialQualityLevel.Num);
+    public FExpressionInput[] Inputs = [];
 
     public override void Deserialize(FAssetArchive Ar, long validPos)
     {
         base.Deserialize(Ar, validPos);
-
-        var toRemove = new List<FPropertyTag>();
-
-        foreach (var property in Properties)
+        if (!TryGetAllValues(out Inputs, nameof(Inputs)))
         {
-            if (property.Tag?.GenericValue is FScriptStruct { StructType: FExpressionInput input } && property.Name.Text == "Inputs")
-            {
-                Inputs.Add(input);
-                toRemove.Add(property);
-            }
+            Inputs = new FExpressionInput[(int) EMaterialQualityLevel.Num];
         }
-
-        foreach (var remove in toRemove)
-        {
-            Properties.Remove(remove);
-        }
-    }
-
-    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
-    {
-        base.WriteJson(writer, serializer);
-
-        writer.WritePropertyName("Inputs");
-        writer.WriteStartArray();
-
-        foreach (var input in Inputs)
-        {
-            serializer.Serialize(writer, input);
-        }
-
-        writer.WriteEndArray();
     }
 }
