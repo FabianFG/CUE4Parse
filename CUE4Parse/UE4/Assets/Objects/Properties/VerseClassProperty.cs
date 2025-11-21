6b456aec2f96eaf180468bf317a7324ed954f2a1
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2025-11-21 15:24:13 +0530

    FVerseClassProperty

--- CUE4Parse/UE4/Assets/Objects/Properties/VerseClassProperty.cs
+++ CUE4Parse/UE4/Assets/Objects/Properties/VerseClassProperty.cs
@@ -1,6 +1,10 @@
-﻿namespace CUE4Parse.UE4.Assets.Objects.Properties;
+﻿using CUE4Parse.UE4.Assets.Readers;
 
-public class VerseClassProperty
+namespace CUE4Parse.UE4.Assets.Objects.Properties;
+
+public class VerseClassProperty : ClassProperty
 {
-    
+    public VerseClassProperty(FAssetArchive Ar, ReadType type) : base(Ar, type)
+    {
+    }
 }
