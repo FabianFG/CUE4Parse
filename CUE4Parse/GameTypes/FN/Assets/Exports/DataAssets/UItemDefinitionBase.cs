a36be2eef223d9ded70c558c629ed023e58788d6
Asval <asval.contactme@gmail.com>
2025-12-14 17:38:53 +0100

    some fn data assets

---
+++ CUE4Parse/GameTypes/FN/Assets/Exports/DataAssets/UItemDefinitionBase.cs
@@ -0,0 +1,16 @@
+﻿using CUE4Parse.UE4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+
+namespace CUE4Parse.GameTypes.FN.Assets.Exports.DataAssets;
+
+public class UItemDefinitionBase : UMcpItemDefinitionBase
+{
+    public FInstancedStruct[] DataList;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        DataList = GetOrDefault<FInstancedStruct[]>(nameof(DataList), []);
+    }
+}
