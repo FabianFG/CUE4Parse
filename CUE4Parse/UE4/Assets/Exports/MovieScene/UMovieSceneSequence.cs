6f1c21d794d661d273594b32bf5572e56efedd26
Asval <asval.contactme@gmail.com>
2025-12-16 01:19:58 +0100

    ULevelSequence

---
+++ CUE4Parse/UE4/Assets/Exports/MovieScene/UMovieSceneSequence.cs
@@ -0,0 +1,16 @@
+﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.UObject;
+
+namespace CUE4Parse.UE4.Assets.Exports.MovieScene;
+
+public class UMovieSceneSequence : UMovieSceneSignedObject
+{
+    public FPackageIndex CompiledData;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        CompiledData = GetOrDefault(nameof(CompiledData), new FPackageIndex());
+    }
+}
---
+++ CUE4Parse/UE4/Assets/Exports/MovieScene/UMovieSceneSignedObject.cs
@@ -0,0 +1,16 @@
+﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Objects.Core.Misc;
+
+namespace CUE4Parse.UE4.Assets.Exports.MovieScene;
+
+public class UMovieSceneSignedObject : UObject
+{
+    public FGuid Signature;
+
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        Signature = GetOrDefault<FGuid>(nameof(Signature));
+    }
+}
