30967f1fa3b51fd60d39b0226336c6777650ee97
Masusder <59669685+Masusder@users.noreply.github.com>
2026-01-31 21:59:13 +0100

    EAkClipAutomationType

---
+++ CUE4Parse/UE4/Wwise/Enums/EAkClipAutomationType.cs
@@ -0,0 +1,10 @@
+namespace CUE4Parse.UE4.Wwise.Enums;
+
+public enum EAkClipAutomationType : uint
+{
+    Volume = 0x0,
+    LPF = 0x1,
+    HPF = 0x2,
+    FadeIn = 0x3,
+    FadeOut = 0x4,
+};
--- CUE4Parse/UE4/Wwise/Objects/AkClipAutomation.cs
+++ CUE4Parse/UE4/Wwise/Objects/AkClipAutomation.cs
@@ -1,17 +1,21 @@
 using CUE4Parse.UE4.Readers;
+using CUE4Parse.UE4.Wwise.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace CUE4Parse.UE4.Wwise.Objects;
 
 public readonly struct AkClipAutomation
 {
-    public readonly uint UClipIndex;
-    public readonly uint EAutoType;
+    public readonly uint ClipIndex;
+    [JsonConverter(typeof(StringEnumConverter))]
+    public readonly EAkClipAutomationType AutoType;
     public readonly AkRtpcGraphPoint[] GraphPoints;
 
     public AkClipAutomation(FArchive Ar)
     {
-        UClipIndex = Ar.Read<uint>();
-        EAutoType = Ar.Read<uint>();
+        ClipIndex = Ar.Read<uint>();
+        AutoType = Ar.Read<EAkClipAutomationType>();
         GraphPoints = Ar.ReadArray((int) Ar.Read<uint>(), () => new AkRtpcGraphPoint(Ar));
     }
 }
