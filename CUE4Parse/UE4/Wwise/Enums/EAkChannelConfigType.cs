f5ed39b86f6a8a5d5a6311a4af4ff39c40fa7af3
Masusder <59669685+Masusder@users.noreply.github.com>
2026-02-08 10:12:59 +0100

    Bring back AkChannelConfig

--- CUE4Parse/UE4/Wwise/Enums/EAkChannelConfigType.cs
+++ CUE4Parse/UE4/Wwise/Enums/EAkChannelConfigType.cs
@@ -1,5 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace CUE4Parse.UE4.Wwise.Enums;
 
+[JsonConverter(typeof(StringEnumConverter))]
 public enum EAkChannelConfigType : uint
 {
     Anonymous = 0x0,
