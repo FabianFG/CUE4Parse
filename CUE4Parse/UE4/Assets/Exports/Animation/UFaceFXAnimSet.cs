aed9b41f8f1f9203161bd47226a552859efebaab
Masusder <59669685+Masusder@users.noreply.github.com>
2026-02-14 20:23:53 +0100

    Borderlands 4

--- CUE4Parse/UE4/Assets/Exports/Animation/UFaceFXAnimSet.cs
+++ CUE4Parse/UE4/Assets/Exports/Animation/UFaceFXAnimSet.cs
@@ -1,15 +1,29 @@
-﻿using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.GameTypes.Borderlands4.Assets.Objects;
+using CUE4Parse.UE4.Assets.Readers;
+using CUE4Parse.UE4.Versions;
+using Newtonsoft.Json;
 
-namespace CUE4Parse.UE4.Assets.Exports.Animation
+namespace CUE4Parse.UE4.Assets.Exports.Animation;
+
+public class UFaceFXAnimSet : UObject
 {
-    public abstract class UFaceFXAnimSet : UObject
-    {
-        public override void Deserialize(FAssetArchive Ar, long validPos)
-        {
-            base.Deserialize(Ar, validPos);
+    public GbxFaceFXAnimData[] FaceFXAnimDataList = [];
+    [JsonIgnore]
+    public byte[] AnimBuffer = [];
 
-            Ar.ReadArray(() => Ar.ReadArray<byte>()); // RawFaceFXAnimSetBytes
-            Ar.ReadArray(() => Ar.ReadArray<byte>()); // RawFaceFXMiniSessionBytes
-        }
+    public override void Deserialize(FAssetArchive Ar, long validPos)
+    {
+        base.Deserialize(Ar, validPos);
+
+        if (Ar.Game is not EGame.GAME_Borderlands4)
+            return;
+
+        FaceFXAnimDataList = GetOrDefault<GbxFaceFXAnimData[]>(nameof(FaceFXAnimDataList)) ?? [];
+        AnimBuffer = GetOrDefault<byte[]>(nameof(AnimBuffer));
+    }
+
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+    {
+        base.WriteJson(writer, serializer);
     }
 }
