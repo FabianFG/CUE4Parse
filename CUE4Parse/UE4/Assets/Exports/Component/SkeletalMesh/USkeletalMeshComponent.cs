ae9c85c8b6bae523b3194be29c72ea889f801a50
Asval <asval.contactme@gmail.com>
2026-03-06 18:34:57 +0100

    USkeletalMeshComponent.AnimationData

--- CUE4Parse/UE4/Assets/Exports/Component/SkeletalMesh/USkeletalMeshComponent.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/SkeletalMesh/USkeletalMeshComponent.cs
@@ -1,5 +1,4 @@
 using CUE4Parse.UE4.Assets.Readers;
-using CUE4Parse.UE4.Objects.UObject;
 using CUE4Parse.UE4.Versions;
 
 namespace CUE4Parse.UE4.Assets.Exports.Component.SkeletalMesh;
@@ -8,48 +7,13 @@ public class USkeletalMeshComponentBudgeted : USkeletalMeshComponent;
 
 public class USkeletalMeshComponent : USkinnedMeshComponent
 {
+    public FSingleAnimationPlayData? AnimationData { get; private set; }
+
     public override void Deserialize(FAssetArchive Ar, long validPos)
     {
         base.Deserialize(Ar, validPos);
+        AnimationData = GetOrDefault<FSingleAnimationPlayData>(nameof(AnimationData));
+
         if(Ar.Game == EGame.GAME_WorldofJadeDynasty) Ar.Position += 20;
-    }
-
-    public FPackageIndex GetSkeletalMesh()
-    {
-        var skeletalMesh = GetSkeletalMesh("SkeletalMesh"); // deprecated in 5.1 so fallback below
-        if (skeletalMesh.IsNull) skeletalMesh = GetSkeletalMesh("SkinnedAsset");
-
-        return skeletalMesh;
-    }
-
-    public FPackageIndex GetSkeletalMesh(string parameterName)
-    {
-        var mesh = new FPackageIndex();
-        var current = this;
-        while (true)
-        {
-            if (current is null) break;
-            mesh = current.GetOrDefault(parameterName, new FPackageIndex());
-            if (!mesh.IsNull || current.Template == null)
-                break;
-            current = current.Template.Load<USkeletalMeshComponent>();
-        }
-
-        return mesh;
-    }
-
-    public bool SetSkeletalMeshIfNull(FPackageIndex mesh)
-    {
-        if (GetSkeletalMesh().IsNull)
-        {
-            SetSkeletalMesh(mesh);
-            return true;
-        }
-        return false;
-    }
-
-    public void SetSkeletalMesh(FPackageIndex mesh)
-    {
-        PropertyUtil.Set(this, "SkeletalMesh", mesh);
     }
 }
