d957d77e59d0eb10f42e7abbd2ae84e6dc243f9c
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2026-03-27 02:46:19 +1100

    fixed

--- CUE4Parse/UE4/Assets/Exports/Animation/FReferenceSkeleton.cs
+++ CUE4Parse/UE4/Assets/Exports/Animation/FReferenceSkeleton.cs
@@ -27,17 +27,9 @@ public class FReferenceSkeleton
         {
             FinalRefBoneInfo = Ar.ReadArray(() => new FMeshBoneInfo(Ar));
             FinalRefBonePose = Ar.ReadArray(() => new FTransform(Ar));
-            FinalNameToIndexMap = Ar.Ver >= EUnrealEngineObjectUE4Version.REFERENCE_SKELETON_REFACTOR ? Ar.ReadMap(() => Ar.ReadFName().Text, Ar.Read<int>) : [];
         }
 
-        if (Ar.Ver >= EUnrealEngineObjectUE4Version.REFERENCE_SKELETON_REFACTOR)
-        {
-            FinalNameToIndexMap = Ar.ReadMap(() => Ar.ReadFName().Text, Ar.Read<int>);
-        }
-        else
-        {
-            FinalNameToIndexMap = [];
-        }
+        FinalNameToIndexMap = Ar.Ver >= EUnrealEngineObjectUE4Version.REFERENCE_SKELETON_REFACTOR ? Ar.ReadMap(() => Ar.ReadFName().Text, Ar.Read<int>) : [];
 
         if (Ar.Game == EGame.GAME_DaysGone) Ar.SkipFixedArray(12);
 
