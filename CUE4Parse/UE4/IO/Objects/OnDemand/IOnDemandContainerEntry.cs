fa9be3d1897fdefdfbcb1154931187abb6b6fb6f
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2026-05-20 22:00:40 +0530

    Something is going wrong while downloading

---
+++ CUE4Parse/UE4/IO/Objects/OnDemand/IOnDemandContainerEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CUE4Parse.UE4.Objects.Core.Misc;
+
+namespace CUE4Parse.UE4.IO.Objects.OnDemand;
+
+public interface IOnDemandContainerEntry
+{
+    public FSHAHash UTocHash { get; }
+
+    public string GetContainerName();
+    public bool TryGetFileEntryHash(FIoChunkId chunkId, out FSHAHash containerHash);
+}
---
+++ CUE4Parse/UE4/IO/Objects/OnDemand/V1/FOnDemandToc.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CUE4Parse.UE4.Readers;
+
+namespace CUE4Parse.UE4.IO.Objects.OnDemand.V1;
+
+public class FOnDemandToc : IOnDemandToc
+{
+    public string ChunksDirectory => Header.ChunksDirectory;
+    public IReadOnlyList<IOnDemandContainerEntry> Containers { get; }
+
+    private readonly FOnDemandTocHeader Header;
+    public FTocMeta? TocMeta;
+    
+    public FOnDemandToc(FArchive Ar)
+    {
+        Header = new FOnDemandTocHeader(Ar);
+        if (Header.Version >= EOnDemandTocVersion.Meta)
+            TocMeta = new FTocMeta(Ar);
+    }
+}
