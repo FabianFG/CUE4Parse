6929ca2ca2837fa18beed7a20ba80235b418871e
Minshu <65584814+MinshuG@users.noreply.github.com>
2026-06-02 12:07:16 +0530

    GeometryCollection

---
+++ CUE4Parse/UE4/Objects/Chaos/GeometryCollection/FGeometryCollectionSection.cs
@@ -0,0 +1,13 @@
+﻿namespace CUE4Parse.UE4.Assets.Exports.Chaos.GeometryCollection;
+
+public struct FGeometryCollectionSection: IUStruct
+{
+    /** The index of the material with which to render this section. */
+    public int MaterialID;
+
+    /** Range of vertices and indices used when rendering this section. */
+    public int FirstIndex;
+    public int NumTriangles;
+    public int MinVertexIndex;
+    public int MaxVertexIndex;
+}
