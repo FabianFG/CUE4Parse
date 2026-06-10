85130b7304ee7d29f487f78bc1177a97f81ea886
jason <jasonsahin3@gmail.com>
2026-06-10 21:10:08 +0100

    Build the blueprint control-flow graph from Kismet bytecode

--- CUE4Parse/UE4/Objects/UObject/BlueprintDecompiler/Cfg/BlueprintCfg.cs
+++ CUE4Parse/UE4/Objects/UObject/BlueprintDecompiler/Cfg/BlueprintCfg.cs
@@ -7,6 +7,20 @@ public static class BlueprintCfg
     public static bool TryStructure(UFunction function, List<int> jumpTargets, out string body)
     {
         body = string.Empty;
+
+        ControlFlowGraph? cfg;
+        try
+        {
+            cfg = ControlFlowGraph.Build(function);
+        }
+        catch
+        {
+            return false;
+        }
+
+        if (cfg is null)
+            return false;
+
         return false;
     }
 }
---
+++ CUE4Parse/UE4/Objects/UObject/BlueprintDecompiler/Cfg/ControlFlowGraph.cs
@@ -0,0 +1,185 @@
+using System.Collections.Generic;
+using CUE4Parse.UE4.Kismet;
+
+namespace CUE4Parse.UE4.Objects.UObject.BlueprintDecompiler.Cfg;
+
+internal sealed class ControlFlowGraph
+{
+    public readonly KismetExpression[] Statements;
+    public readonly BasicBlock[] Blocks;
+    public readonly int EntryIndex;
+    public readonly int ExitIndex;
+
+    private ControlFlowGraph(KismetExpression[] statements, BasicBlock[] blocks, int entryIndex, int exitIndex)
+    {
+        Statements = statements;
+        Blocks = blocks;
+        EntryIndex = entryIndex;
+        ExitIndex = exitIndex;
+    }
+
+    public static ControlFlowGraph? Build(UFunction function)
+    {
+        var code = function.ScriptBytecode;
+        if (code == null || code.Length == 0)
+            return null;
+
+        var offsetToIndex = new Dictionary<int, int>(code.Length);
+        for (var i = 0; i < code.Length; i++)
+            offsetToIndex[code[i].StatementIndex] = i;
+
+        var leaders = new SortedSet<int> { 0 };
+        for (var i = 0; i < code.Length; i++)
+        {
+            switch (code[i])
+            {
+                case EX_ComputedJump:
+                    return null;
+                case EX_JumpIfNot jumpIfNot:
+                    if (!offsetToIndex.TryGetValue((int) jumpIfNot.CodeOffset, out var jumpIfNotTarget)) return null;
+                    leaders.Add(jumpIfNotTarget);
+                    if (i + 1 < code.Length) leaders.Add(i + 1);
+                    break;
+                case EX_Skip:
+                    break;
+                case EX_Jump jump:
+                    if (!offsetToIndex.TryGetValue((int) jump.CodeOffset, out var jumpTarget)) return null;
+                    leaders.Add(jumpTarget);
+                    if (i + 1 < code.Length) leaders.Add(i + 1);
+                    break;
+                case EX_Return:
+                case EX_EndOfScript:
+                    if (i + 1 < code.Length) leaders.Add(i + 1);
+                    break;
+                case EX_PushExecutionFlow push:
+                    if (!offsetToIndex.TryGetValue((int) push.PushingAddress, out var pushTarget)) return null;
+                    leaders.Add(pushTarget);
+                    break;
+                case EX_PopExecutionFlow:
+                case EX_PopExecutionFlowIfNot:
+                    if (i + 1 < code.Length) leaders.Add(i + 1);
+                    break;
+            }
+        }
+
+        var leaderList = new List<int>(leaders);
+        var indexToBlock = new int[code.Length];
+        var blockList = new List<BasicBlock>(leaderList.Count + 1);
+        for (var b = 0; b < leaderList.Count; b++)
+        {
+            var start = leaderList[b];
+            var end = (b + 1 < leaderList.Count ? leaderList[b + 1] : code.Length) - 1;
+            blockList.Add(new BasicBlock(b, start, end));
+            for (var i = start; i <= end; i++)
+                indexToBlock[i] = b;
+        }
+
+        var exitIndex = blockList.Count;
+        blockList.Add(new BasicBlock(exitIndex, -1, -1));
+        var blocks = blockList.ToArray();
+
+        var entryStacks = new FlowStack?[blocks.Length];
+        var visited = new bool[blocks.Length];
+        var queue = new Queue<int>();
+        visited[0] = true;
+        queue.Enqueue(0);
+
+        while (queue.Count > 0)
+        {
+            var bi = queue.Dequeue();
+            if (bi == exitIndex)
+                continue;
+
+            var block = blocks[bi];
+            var stack = entryStacks[bi];
+            for (var i = block.Start; i <= block.End; i++)
+            {
+                if (code[i] is EX_PushExecutionFlow push)
+                    stack = new FlowStack((int) push.PushingAddress, stack);
+            }
+
+            var successors = new List<(int Block, FlowStack? Stack)>(2);
+            switch (code[block.End])
+            {
+                case EX_JumpIfNot jumpIfNot:
+                    successors.Add((bi + 1, stack));
+                    successors.Add((indexToBlock[offsetToIndex[(int) jumpIfNot.CodeOffset]], stack));
+                    break;
+                case EX_Skip:
+                    successors.Add((bi + 1, stack));
+                    break;
+                case EX_Jump jump:
+                    successors.Add((indexToBlock[offsetToIndex[(int) jump.CodeOffset]], stack));
+                    break;
+                case EX_Return:
+                case EX_EndOfScript:
+                    successors.Add((exitIndex, stack));
+                    break;
+                case EX_PopExecutionFlow:
+                    if (stack is null) successors.Add((exitIndex, null));
+                    else successors.Add((indexToBlock[offsetToIndex[stack.Value]], stack.Tail));
+                    break;
+                case EX_PopExecutionFlowIfNot:
+                    successors.Add((bi + 1, stack));
+                    if (stack is null) successors.Add((exitIndex, null));
+                    else successors.Add((indexToBlock[offsetToIndex[stack.Value]], stack.Tail));
+                    break;
+                default:
+                    successors.Add((bi + 1, stack));
+                    break;
+            }
+
+            foreach (var (succ, succStack) in successors)
+            {
+                if (!block.Successors.Contains(succ))
+                {
+                    block.Successors.Add(succ);
+                    blocks[succ].Predecessors.Add(bi);
+                }
+
+                if (!visited[succ])
+                {
+                    visited[succ] = true;
+                    entryStacks[succ] = succStack;
+                    queue.Enqueue(succ);
+                }
+                else if (!FlowStack.Equal(entryStacks[succ], succStack))
+                {
+                    return null;
+                }
+            }
+        }
+
+        for (var b = 0; b < exitIndex; b++)
+        {
+            if (!visited[b])
+                return null;
+        }
+
+        return new ControlFlowGraph(code, blocks, 0, exitIndex);
+    }
+
+    private sealed class FlowStack
+    {
+        public readonly int Value;
+        public readonly FlowStack? Tail;
+
+        public FlowStack(int value, FlowStack? tail)
+        {
+            Value = value;
+            Tail = tail;
+        }
+
+        public static bool Equal(FlowStack? a, FlowStack? b)
+        {
+            while (a is not null && b is not null)
+            {
+                if (a.Value != b.Value) return false;
+                a = a.Tail;
+                b = b.Tail;
+            }
+
+            return a is null && b is null;
+        }
+    }
+}
