1e22952a60e818c6a9baacc1d02c7f4b37da3323
jason <jasonsahin3@gmail.com>
2026-06-11 15:42:34 +0100

    Log when control-flow structuring falls back to goto emission

--- CUE4Parse/UE4/Objects/UObject/BlueprintDecompiler/Cfg/BlueprintCfg.cs
+++ CUE4Parse/UE4/Objects/UObject/BlueprintDecompiler/Cfg/BlueprintCfg.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Serilog;
 
 namespace CUE4Parse.UE4.Objects.UObject.BlueprintDecompiler.Cfg;
 
@@ -12,25 +14,38 @@ public static class BlueprintCfg
         {
             var cfg = ControlFlowGraph.Build(function, entryOffsets);
             if (cfg is null)
+            {
+                Log.Debug("Structuring fell back to goto for {Function}: control-flow graph could not be built (irreducible or computed jump)", function.Name);
                 return false;
+            }
 
             var dom = Dominators.Compute(cfg);
             var structurer = Structurer.Structure(cfg, dom, out var root);
             if (structurer is null || root is null)
+            {
+                Log.Debug("Structuring fell back to goto for {Function}: structurer refused the region", function.Name);
                 return false;
+            }
 
             if (!CfgEquivalence.Verify(cfg, root))
+            {
+                Log.Debug("Structuring fell back to goto for {Function}: structured output failed the equivalence check", function.Name);
                 return false;
+            }
 
             var folded = SwitchFold.Fold(root, cfg);
             if (SwitchFold.Depth(folded) > MaxOutputNesting)
+            {
+                Log.Debug("Structuring fell back to goto for {Function}: output nesting exceeded {Max}", function.Name, MaxOutputNesting);
                 return false;
+            }
 
             new StructuredEmitter(cfg, structurer.GotoTargets, FoldPlan.Compute(cfg), builder).Emit(folded);
             return true;
         }
-        catch
+        catch (Exception e)
         {
+            Log.Debug(e, "Structuring threw for {Function}; falling back to goto", function.Name);
             return false;
         }
     }
