5ee88ee3a089436eec788451108b300539bb8420
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2026-06-27 03:37:47 +1000

    Misc

--- CUE4Parse/GameTypes/ProSpi/Assets/Exports/USpectatorMesh.cs
+++ CUE4Parse/GameTypes/ProSpi/Assets/Exports/USpectatorMesh.cs
@@ -1,18 +1,5 @@
 using CUE4Parse.UE4.Assets.Exports;
-using CUE4Parse.UE4.Assets.Readers;
-using Newtonsoft.Json;
 
 namespace CUE4Parse.GameTypes.ProSpi.Assets.Exports;
 
-public class USpectatorMesh : UObject
-{
-    public override void Deserialize(FAssetArchive Ar, long validPos)
-    {
-        base.Deserialize(Ar, validPos);
-    }
-
-    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
-    {
-        base.WriteJson(writer, serializer);
-    }
-}
+public class USpectatorMesh : UObject;
