82c96111e21508306b98ba209b139a4fe9322c50
LongerWarrior <LongerWarrior@gmail.com>
2026-06-30 01:21:37 +0300

    FArchiveBigEndian refactor

--- CUE4Parse/UE4/Readers/FArchiveBigEndian.cs
+++ CUE4Parse/UE4/Readers/FArchiveBigEndian.cs
@@ -1,97 +1,115 @@
 using System.Buffers.Binary;
+using System.Collections.Concurrent;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using CommunityToolkit.HighPerformance;
 using CUE4Parse.UE4.Exceptions;
 
 namespace CUE4Parse.UE4.Readers;
 
-// Thank you RedHaze
 public class FArchiveBigEndian : FArchive
 {
-    private FArchive _baseArchive;
+    private readonly FArchive _baseArchive;
 
     public FArchiveBigEndian(FArchive baseArchive) : base(baseArchive.Versions)
     {
         _baseArchive = baseArchive;
     }
 
-    private static readonly Dictionary<Type, Func<FArchiveBigEndian, object>> _read = new()
+    private const BindingFlags FieldFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+    private record struct LayoutFixup(ushort Offset, ushort Size, ushort Count);
+    private static readonly ConcurrentDictionary<Type, LayoutFixup[]> Layouts = new();
+
+    private static readonly Dictionary<Type, int> PrimitiveTypes = new()
     {
-        { typeof(short),  ar => BinaryPrimitives.ReadInt16BigEndian(ar.ReadBytes(sizeof(short))) },
-        { typeof(int),    ar => BinaryPrimitives.ReadInt32BigEndian(ar.ReadBytes(sizeof(int))) },
-        { typeof(long),   ar => BinaryPrimitives.ReadInt64BigEndian(ar.ReadBytes(sizeof(long))) },
-        { typeof(ushort), ar => BinaryPrimitives.ReadUInt16BigEndian(ar.ReadBytes(sizeof(ushort))) },
-        { typeof(uint),   ar => BinaryPrimitives.ReadUInt32BigEndian(ar.ReadBytes(sizeof(uint))) },
-        { typeof(ulong),  ar => BinaryPrimitives.ReadUInt64BigEndian(ar.ReadBytes(sizeof(ulong))) },
-        { typeof(float),  ar => BinaryPrimitives.ReadSingleBigEndian(ar.ReadBytes(sizeof(float))) },
-        { typeof(double), ar => BinaryPrimitives.ReadDoubleBigEndian(ar.ReadBytes(sizeof(double))) },
+        [typeof(byte)] = 1,
+        [typeof(sbyte)] = 1,
+        [typeof(bool)] = 1,
+
+        [typeof(short)] = 2,
+        [typeof(ushort)] = 2,
+        [typeof(char)] = 2,
+        [typeof(Half)] = 2,
+
+        [typeof(int)] = 4,
+        [typeof(uint)] = 4,
+        [typeof(float)] = 4,
+
+        [typeof(long)] = 8,
+        [typeof(ulong)] = 8,
+        [typeof(double)] = 8,
+        [typeof(nint)] = 8,
+        [typeof(nuint)] = 8,
     };
+
+    public sealed override T Read<T>()
+    {
+        var size = Unsafe.SizeOf<T>();
+        Span<byte> span = stackalloc byte[size];
+        Read(span);
+
+        var layout = Layouts.GetOrAdd(typeof(T), BuildLayout);
+        foreach (var field in layout)
+        {
+            int start = field.Offset;
+            for (var i = 0; i < field.Count; i++)
+            {
+                var end = start + field.Size;
+                span[start..end].Reverse();
+                start = end;
+            }
+        }
+        return Unsafe.ReadUnaligned<T>(ref span[0]);
+    }
 
     public override int Read(byte[] buffer, int offset, int count) => _baseArchive.Read(buffer, offset, count);
     public override string ReadString() => Encoding.UTF8.GetString(ReadArray<byte>());
     public override long Seek(long offset, SeekOrigin origin) => _baseArchive.Seek(offset, origin);
 
-    public sealed override T Read<T>()
-    {
-        if (_read.TryGetValue(typeof(T), out var func))
-            return (T)func(this);
-
-        return base.Read<T>();
-    }
-
-    public override T[] ReadArray<T>(int length)
+    public sealed override T[] ReadArray<T>(int length)
     {
         var size = Unsafe.SizeOf<T>();
         var readLength = size * length;
         CheckReadSize(readLength);
 
-        var buffer = ReadBytes(readLength);
-        if (size == 1)
+        if (PrimitiveTypes.TryGetValue(typeof(T), out _))
         {
+            var buffer = ReadBytes(readLength);
+            ReverseEndian(buffer, size);
+            var result = new T[length];
+            if (length > 0) Unsafe.CopyBlockUnaligned(ref Unsafe.As<T, byte>(ref result[0]), ref buffer[0], (uint)(readLength));
+            return result;
         }
-        else if (size == 2)
-        {
-            ReverseEndian(buffer.AsSpan().Cast<byte, ushort>());
-        }
-        else if (typeof(T) == typeof(uint) || typeof(T) == typeof(int) || typeof(T) == typeof(float))
-        {
-            ReverseEndian(buffer.AsSpan().Cast<byte, uint>());
-        }
-        else if (typeof(T) == typeof(ulong) || typeof(T) == typeof(long) || typeof(T) == typeof(double))
-        {
-            ReverseEndian(buffer.AsSpan().Cast<byte, ulong>());
-        }
-        else
-        {
-            throw new ParserException("Unsupported type for ReadArray: " + typeof(T).Name);
-        }
-        var result = new T[length];
-        if (length > 0) Unsafe.CopyBlockUnaligned(ref Unsafe.As<T, byte>(ref result[0]), ref buffer[0], (uint)(readLength));
-        return result;
+
+        var result1 = new T[length];
+        for (var i = 0; i < length; i++)
+            result1[i] = Read<T>();
+        return result1;
     }
 
-    static void ReverseEndian<TSwap>(Span<TSwap> span) where TSwap : unmanaged
+    static void ReverseEndian(byte[] span, int size)
     {
-        if (typeof(TSwap) == typeof(ushort))
+        if (size == 2)
         {
-            var span2 = span.Cast<TSwap, ushort>();
+            var span2 = span.AsSpan().Cast<byte, ushort>();
             for (int i = 0; i < span.Length; i++)
             {
                 span2[i] = BinaryPrimitives.ReverseEndianness(span2[i]);
             }
         }
-        else if (typeof(TSwap) == typeof(uint))
+        else if (size == 4)
         {
-            var span4 = span.Cast<TSwap, uint>();
+            var span4 = span.AsSpan().Cast<byte, uint>();
             for (int i = 0; i < span.Length; i++)
             {
                 span4[i] = BinaryPrimitives.ReverseEndianness(span4[i]);
             }
         }
-        else if (typeof(TSwap) == typeof(ulong))
+        else if (size == 8)
         {
-            var span8 = span.Cast<TSwap, ulong>();
+            var span8 = span.AsSpan().Cast<byte, ulong>();
             for (int i = 0; i < span.Length; i++)
             {
                 span8[i] = BinaryPrimitives.ReverseEndianness(span8[i]);
@@ -109,4 +127,95 @@ public class FArchiveBigEndian : FArchive
     }
 
     public override object Clone() => new FArchiveBigEndian(_baseArchive);
+
+    private static LayoutFixup[] BuildLayout(Type type)
+    {
+        if (type.IsEnum)
+        {
+            type = Enum.GetUnderlyingType(type);
+        }
+
+        if (PrimitiveTypes.TryGetValue(type, out int primitiveSize))
+        {
+            return [new LayoutFixup(0, (ushort)primitiveSize, 1)];
+        }
+
+        List<LayoutFixup> layoutFixups = [];
+        foreach (var field in type.GetFields(FieldFlags).OrderBy(x => x.MetadataToken))
+        {
+            int offset = (int)Marshal.OffsetOf(type, field.Name);
+            Type fieldType = field.FieldType;
+            if (!fieldType.IsValueType)
+                throw new ParserException($"FArchiveBigEndian can't read reference type {fieldType} in {type}");
+
+            var inline = fieldType.GetCustomAttribute<InlineArrayAttribute>();
+            if (inline != null)
+            {
+                var element = fieldType.GetFields(FieldFlags).Single().FieldType;
+
+                if (PrimitiveTypes.TryGetValue(element, out var size1))
+                {
+                    layoutFixups.Add(new LayoutFixup((ushort) offset, (ushort) size1, (ushort) inline.Length));
+                }
+                else
+                {
+                    var temp = Layouts.GetOrAdd(fieldType, BuildLayout(fieldType));
+                    var tempOffset = offset;
+                    var tempsize = Marshal.SizeOf(fieldType);
+                    for (var i = 0; i < inline.Length; i++)
+                    {
+                        foreach (var fix in temp)
+                        {
+                            layoutFixups.Add(fix with { Offset = (ushort)(fix.Offset + tempOffset) });
+                        }
+                        tempOffset += tempsize;
+                    }
+                }
+
+                continue;
+            }
+
+            if (PrimitiveTypes.TryGetValue(fieldType, out int size))
+            {
+                layoutFixups.Add(new LayoutFixup((ushort)offset, (ushort)size, 1));
+            }
+            else
+            {
+                var temp = Layouts.GetOrAdd(fieldType, BuildLayout(fieldType));
+                foreach (var fix in temp)
+                {
+                    layoutFixups.Add(fix with { Offset = (ushort)(fix.Offset + offset) });
+                }
+            }
+        }
+
+        return CompactLayout(layoutFixups);
+    }
+
+    private static LayoutFixup[] CompactLayout(List<LayoutFixup> fixups)
+    {
+        if (fixups.Count == 0) return [];
+
+        fixups.Sort((a, b) => a.Offset.CompareTo(b.Offset));
+        var result = new List<LayoutFixup>(fixups.Count);
+        var current = fixups[0];
+
+        for (int i = 1; i < fixups.Count; i++)
+        {
+            var next = fixups[i];
+            if (current.Offset + current.Size * current.Count == next.Offset && current.Size == next.Size)
+            {
+                current.Count += next.Count;
+            }
+            else
+            {
+                result.Add(current);
+                current = next;
+            }
+        }
+
+        result.Add(current);
+
+        return result.ToArray();
+    }
 }
