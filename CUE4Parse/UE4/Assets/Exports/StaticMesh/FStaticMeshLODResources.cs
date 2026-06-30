996574cc1005f1b2e314e377c7a7b3fd42f412f8
Masusder <59669685+Masusder@users.noreply.github.com>
2026-06-30 17:43:14 +0200

    Fixed LOM meshes

--- CUE4Parse/UE4/Assets/Exports/StaticMesh/FStaticMeshLODResources.cs
+++ CUE4Parse/UE4/Assets/Exports/StaticMesh/FStaticMeshLODResources.cs
@@ -83,6 +83,7 @@ public class FStaticMeshLODResources
         if (!stripDataFlags.IsAudioVisualDataStripped() && !bIsLODCookedOut)
         {
             if (Ar.Game >= EGame.GAME_UE5_5 || Ar.Game == EGame.GAME_MetalGearSolidDelta) Ar.Position += 4; // bHasRayTracingGeometry
+            if (Ar.Game is EGame.GAME_LordOfMysteries) Ar.Position += 4;
 
             if (bInlined)
             {
