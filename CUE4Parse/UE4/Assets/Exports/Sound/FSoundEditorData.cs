7b5dd83e4e5bccea712968477dbe19e51d0d6117
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2026-07-02 05:05:08 +1000

    USoundCue and USoundClass changes

---
+++ CUE4Parse/UE4/Assets/Exports/Sound/FSoundEditorData.cs
@@ -0,0 +1,7 @@
+﻿namespace CUE4Parse.UE4.Assets.Exports.Sound;
+
+public struct FSoundEditorData
+{
+    public int X;
+    public int Y;
+}
--- CUE4Parse/UE4/Assets/Exports/Sound/USoundClass.cs
+++ CUE4Parse/UE4/Assets/Exports/Sound/USoundClass.cs
@@ -1,25 +1,32 @@
 using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.UObject;
 using CUE4Parse.UE4.Versions;
+using Newtonsoft.Json;
 
 namespace CUE4Parse.UE4.Assets.Exports.Sound;
 
-public struct FSoundClassEditorData
-{
-    public int X;
-    public int Y;
-}
-
 public class USoundClass : UObject
 {
-    public KeyValuePair<FPackageIndex, FSoundClassEditorData>[]? EditorData;
+    public Dictionary<FPackageIndex, FSoundEditorData>? EditorData;
 
     public override void Deserialize(FAssetArchive Ar, long validPos)
     {
         base.Deserialize(Ar, validPos);
+
         if (Ar.Ver >= EUnrealEngineObjectUE3Version.SOUND_CLASS_SERIALISATION_UPDATE && Ar.Ver < EUnrealEngineObjectUE4Version.SOUND_CLASS_GRAPH_EDITOR)
         {
-            EditorData = Ar.ReadArray<KeyValuePair<FPackageIndex, FSoundClassEditorData>>(() => new(new FPackageIndex(Ar), Ar.Read<FSoundClassEditorData>()));
+            EditorData = Ar.ReadMap(() => new FPackageIndex(Ar), () => Ar.Read<FSoundEditorData>());
+        }
+    }
+
+    protected internal override void WriteJson(JsonWriter writer, JsonSerializer serializer)
+    {
+        base.WriteJson(writer, serializer);
+
+        if (EditorData?.Count > 0)
+        {
+            writer.WritePropertyName("EditorData");
+            serializer.Serialize(writer, EditorData);
         }
     }
 }
