ff1c7701b29d6de644ed0da760f3de00025e8a88
Masusder <59669685+Masusder@users.noreply.github.com>
2026-07-22 17:41:27 +0200

    That's better
    Co-Authored-By: LongerWarrior <37636768+LongerWarrior@users.noreply.github.com>

--- CUE4Parse/GameTypes/DeadByDaylight/Objects/FBhvrBarkNodeTemplate.cs
+++ CUE4Parse/GameTypes/DeadByDaylight/Objects/FBhvrBarkNodeTemplate.cs
@@ -1,24 +1,17 @@
 using CUE4Parse.UE4;
 using CUE4Parse.UE4.Assets.Objects;
 using CUE4Parse.UE4.Assets.Readers;
-using CUE4Parse.UE4.Objects.UObject;
 
 namespace CUE4Parse.GameTypes.DeadByDaylight.Objects;
 
 public class FBhvrBarkNodeTemplate : IUStruct
 {
-    public FPackageIndex? NodeType;
-    public FStructFallback? Node;
+    public FScriptStruct? Node;
 
     public FBhvrBarkNodeTemplate() { }
     public FBhvrBarkNodeTemplate(FAssetArchive Ar)
     {
         if (!Ar.ReadBoolean()) return;
-
-        NodeType = new FPackageIndex(Ar);
-
-        if (NodeType.IsNull) return;
-
-        Node = new FStructFallback(Ar, NodeType.Name);
+        Node = FScriptStruct.ReadInstancedStructWithoutSerialSize(Ar);
     }
 }
