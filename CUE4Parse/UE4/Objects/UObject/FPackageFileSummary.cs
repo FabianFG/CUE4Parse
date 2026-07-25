236351b4a0c9b1a60edd03915f72e40305a35d27
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2026-07-26 04:15:18 +1000

    no more struct read

--- CUE4Parse/UE4/Objects/UObject/FPackageFileSummary.cs
+++ CUE4Parse/UE4/Objects/UObject/FPackageFileSummary.cs
@@ -359,7 +359,7 @@ namespace CUE4Parse.UE4.Objects.UObject
                 }
             }
 
-            Generations = Ar.ReadArray<FGenerationInfo>();
+            Generations = Ar.ReadArray(() => new FGenerationInfo(Ar));
 
             if (FileVersionUE >= EUnrealEngineObjectUE4Version.ENGINE_VERSION_OBJECT)
             {
