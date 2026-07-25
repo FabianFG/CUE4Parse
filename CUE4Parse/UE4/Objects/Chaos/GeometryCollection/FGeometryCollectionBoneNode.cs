5d577bd3ea4ce0ebe175d012585d66525c9da06a
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2026-07-25 23:45:08 +0530

    More ManagedArray types

---
+++ CUE4Parse/UE4/Objects/Chaos/GeometryCollection/FGeometryCollectionBoneNode.cs
@@ -0,0 +1,30 @@
+﻿namespace CUE4Parse.UE4.Objects.Chaos.GeometryCollection;
+
+public readonly struct FGeometryCollectionBoneNode
+{
+    public readonly int[] Children;
+    public readonly int Level;
+    public readonly int Parent;
+    public readonly ENodeFlags StatusFlags;
+
+    public FGeometryCollectionBoneNode(FChaosArchive Ar)
+    {
+        Level = Ar.Read<int>();
+        Parent = Ar.Read<int>();
+        Children = Ar.ReadArray<int>();
+        StatusFlags = Ar.Read<ENodeFlags>();
+    }
+}
+
+[Flags]
+public enum ENodeFlags : uint
+{
+    // A node is currently either a geometry node (bit set) or a null node with a transform only (bit zero)
+    FS_Geometry = 0x00000001,
+
+    // additional flags
+    FS_Clustered = 0x00000002,
+
+    // Gets deleted from world instead of becoming a fractured chunk in the world
+    FS_RemoveOnFracture = 0x00000004
+}
--- CUE4Parse/UE4/Objects/Chaos/GeometryCollection/FManagedArrayCollection.cs
+++ CUE4Parse/UE4/Objects/Chaos/GeometryCollection/FManagedArrayCollection.cs
@@ -1,4 +1,5 @@
-﻿using CUE4Parse.UE4.Objects.UObject;
+﻿using CUE4Parse.UE4.Objects.Core.i18N;
+using CUE4Parse.UE4.Objects.UObject;
 
 namespace CUE4Parse.UE4.Objects.Chaos.GeometryCollection;
 
