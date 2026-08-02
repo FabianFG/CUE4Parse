07da06fd95d72c3728020d9b0ed972a7d22e40e4
halfuwu <maxman1346@gmail.com>
2026-08-02 10:44:56 -0400

    feat: add relative transforms to `USceneComponent`

--- CUE4Parse/UE4/Assets/Exports/Component/USceneComponent.cs
+++ CUE4Parse/UE4/Assets/Exports/Component/USceneComponent.cs
@@ -25,11 +25,19 @@ public class USceneComponent : UActorComponent
     public FPackageIndex? AttachParent;
     public FBoxSphereBounds? Bounds;
     public bool bIsCooked;
+    
+    public FVector RelativeLocation;
+    public FRotator RelativeRotation;
+    public FVector RelativeScale3D;
 
     public override void Deserialize(FAssetArchive Ar, long validPos)
     {
         base.Deserialize(Ar, validPos);
         AttachParent = GetOrDefault<FPackageIndex?>(nameof(AttachParent));
+        
+        RelativeLocation = GetOrDefault(nameof(RelativeLocation), FVector.ZeroVector);
+        RelativeRotation = GetOrDefault(nameof(RelativeRotation), FRotator.ZeroRotator);
+        RelativeScale3D = GetOrDefault(nameof(RelativeScale3D), FVector.OneVector);
 
         if (Ar.Game == GAME_WorldofJadeDynasty) Ar.Position += 4;
         var bComputeBoundsOnceForGame = GetOrDefault<bool>("bComputeBoundsOnceForGame");
