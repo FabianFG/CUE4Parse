6bde5715a46f01c85347a7e4f91a354ef71e84aa
halfuwu <maxman1346@gmail.com>
2026-08-02 15:52:15 -0400

    feat: add `NaniteSeparate` option to `ENaniteMeshFormat`

--- CUE4Parse-Conversion/Formats/Meshes/UEFormatMeshFormat.cs
+++ CUE4Parse-Conversion/Formats/Meshes/UEFormatMeshFormat.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using CUE4Parse_Conversion.Dto;
 using CUE4Parse_Conversion.Options;
 using CUE4Parse_Conversion.Writers.UEFormat;
@@ -12,16 +13,35 @@ public sealed class UEFormatMeshFormat : IMeshExportFormat
 
     public IReadOnlyList<ExportFile> BuildSkeletalMesh(string objectName, ExportOptions options, SkeletalMeshDto dto, IReadOnlyDictionary<string, string>? materialPaths = null)
     {
-        using var ar = new FArchiveWriter();
-        new UEModel(objectName, dto, options).Save(ar);
-        return [new ExportFile("uemodel", ar.GetBuffer())];
+        // if we are exporting nanite as a separate lod AND have a split of nanite lod vs. non-nanite lods
+        if (options.NaniteMeshFormat == ENaniteMeshFormat.NaniteSeparate 
+            && dto.LODs.Any(l => l.IsNanite) 
+            && dto.LODs.Any(l => !l.IsNanite))
+        {
+            return
+            [
+                dto.WithLods(lod => !lod.IsNanite, () => Save(objectName, dto, options)),
+                dto.WithLods(lod => lod.IsNanite, () => Save(objectName, dto, options, "_Nanite")),
+            ];
+        }
+
+        return [Save(objectName, dto, options)];
     }
 
     public IReadOnlyList<ExportFile> BuildStaticMesh(string objectName, ExportOptions options, StaticMeshDto dto, IReadOnlyDictionary<string, string>? materialPaths = null)
     {
-        using var ar = new FArchiveWriter();
-        new UEModel(objectName, dto, options).Save(ar);
-        return [new ExportFile("uemodel", ar.GetBuffer())];
+        if (options.NaniteMeshFormat == ENaniteMeshFormat.NaniteSeparate 
+            && dto.LODs.Any(l => l.IsNanite) 
+            && dto.LODs.Any(l => !l.IsNanite))
+        {
+            return
+            [
+                dto.WithLods(lod => !lod.IsNanite, () => Save(objectName, dto, options)),
+                dto.WithLods(lod => lod.IsNanite, () => Save(objectName, dto, options, "_Nanite")),
+            ];
+        }
+
+        return [Save(objectName, dto, options)];
     }
 
     public IReadOnlyList<ExportFile> BuildSkeleton(string objectName, ExportOptions options, SkeletonDto dto)
@@ -30,5 +50,18 @@ public sealed class UEFormatMeshFormat : IMeshExportFormat
         new UEModel(objectName, dto, options).Save(ar);
         return [new ExportFile("uemodel", ar.GetBuffer())];
     }
-}
 
+    private static ExportFile Save(string objectName, StaticMeshDto dto, ExportOptions options, string? suffix = null)
+    {
+        using var ar = new FArchiveWriter();
+        new UEModel(objectName, dto, options).Save(ar);
+        return new ExportFile("uemodel", ar.GetBuffer(), suffix);
+    }
+
+    private static ExportFile Save(string objectName, SkeletalMeshDto dto, ExportOptions options, string? suffix = null)
+    {
+        using var ar = new FArchiveWriter();
+        new UEModel(objectName, dto, options).Save(ar);
+        return new ExportFile("uemodel", ar.GetBuffer(), suffix);
+    }
+}
