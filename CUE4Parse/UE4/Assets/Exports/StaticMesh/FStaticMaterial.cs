ac467792addba6e2bb45c1f262c4d721cf7b7b37
DESKTOP-4JHA683\WM86 <a13561095555@gmail.com>
2026-09-26 16:18:25 +0800

    静态网格：FStaticMaterial 同样补上覆盖材质引用（GAME_SleeplessWilds）

--- CUE4Parse/UE4/Assets/Exports/StaticMesh/FStaticMaterial.cs
+++ CUE4Parse/UE4/Assets/Exports/StaticMesh/FStaticMaterial.cs
@@ -24,7 +24,8 @@ namespace CUE4Parse.UE4.Assets.Exports.StaticMesh
             if (FRenderingObjectVersion.Get(Ar) >= FRenderingObjectVersion.Type.TextureStreamingMeshUVChannelData)
                 UVChannelData = new FMeshUVChannelInfo(Ar);
 
-            if (FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.MeshMaterialSlotOverlayMaterialAdded)
+            // 同 FSkeletalMaterial：GAME_SleeplessWilds 的数据里写了这个 4 字节覆盖材质
+            if (Ar.Game is GAME_SleeplessWilds || FFortniteMainBranchObjectVersion.Get(Ar) >= FFortniteMainBranchObjectVersion.Type.MeshMaterialSlotOverlayMaterialAdded)
             {
                 OverlayMaterialInterface = new FPackageIndex(Ar);
             }
